18de92bdd46e9e67f88d5bbef3f56df403bdc738
ali ahmad <raoaliahmad03@gmail.com>
2019-02-19 16:16:00 +0500

    needs to be finalized

--- web/src/modules/Help/index.js
+++ web/src/modules/Help/index.js
@@ -33,58 +33,58 @@ class Help extends Component {
     
   render() {
 
-    const { sendMessage, smsOption } = this.props;
-    const number = "03360472067"
-
+    const { sendMessage, smsOption, auth } = this.props;
+    const number = "03481112004"
+    const text = `School Name : ${auth.school_id}\nTeacher Name: ${auth.name}\nMessage: ${this.state.text}`
+    
     return (
       <Layout history={this.props.history}>
         <div className="help-page">
           <div className="form" style={{ width: "75%" }}>
             <div className="title">Help</div>
             <div className="section">
-              <h3>PLEASE Contact the following numbers for immediate Help</h3>
-              <li>
-                Call Center - <a href="tel:+923360472067">0336-0472067</a>
-              </li>
-              <h3>or Visit our Website for more Information</h3>
-              <li>
-                Website - <strong> <a href="https://google.com">mischool.pk </a></strong>
-              </li>
+              <div style={{width: "inherit"}}>
+                <h3>If you need any assistance, please call our customer representative or message us using the box below</h3>
+                <div>اگر آپ کو کسی بھی قسم کی مدد کی ضرورت ہو، تو برائے مہربانی ہمارے کسٹمر نمایندہ کو کال کریں یا نیچے دیے گئے باکس سے پیغام بھیجیں، شکریہ۔</div>
+              </div>
+
+              <div style={{marginTop: "30px"}}>
+                <li>
+                  Customer Representative - <a href="tel:+923481112004">0348-111-2004</a>
+                </li>
+              </div>
             </div>
-              
             <div className="divider">Ask Us</div>
               <div className="section">
                 
                 <div className="row">
                 <label>Message</label>
-                <textarea {...this.former.super_handle(["text"])} placeholder="Write text message here" />
+                <textarea {...this.former.super_handle(["text"])} placeholder="Write message here" />
                 </div>
                 <div>
                 {
                   smsOption === "SIM" ? 
                     <a href={smsIntentLink({
-                      messages: [{ number, text: this.state.text }],
+                      messages: [{ number, text }],
                       return_link: window.location.href 
                       })} onClick={this.logSms} className="button blue">Send using Local SIM</a> :
                   
-                    <div className="button" onClick={() => sendMessage( this.state.text, number)}>Can only send using Local SIM</div>
+                    <div className="button" onClick={() => sendMessage( text, number)}>Can only send using Local SIM</div>
                 }
                 </div>
             </div>
           </div>
         </div>
-        
-          
-
       </Layout>
     )
   }
 }
 export default connect(state => ({
-    faculty_id: state.auth.faculty_id,
+  auth: state.auth,
+  faculty_id: state.auth.faculty_id,
 	connected: state.connected,
 	smsOption: state.db.settings.sendSMSOption
 }), dispatch => ({
-	sendMessage: (text, number, type) => dispatch(sendSMS(text, number)),
+	sendMessage: (text, number) => dispatch(sendSMS(text, number)),
 	logSms: (faculty_id, history) => dispatch(logSms(faculty_id, history))
 }))(Help);
