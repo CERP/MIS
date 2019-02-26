6f45356892444906712474fca48baa72105dcdcb
ali ahmad <raoaliahmad03@gmail.com>
2019-02-26 15:05:29 +0500

    Added Logo and new Badge

--- web/src/modules/Landing/index.js
+++ web/src/modules/Landing/index.js
@@ -19,6 +19,7 @@ import classesIcon from './icons/Classes/classes_1.svg'           //
 import settingsIcon from './icons/Settings/settings-gears.svg'    //
 import switchUserIcon from './icons/switch_user/logout.svg'    //no-icon
 import prospective from './icons/Prospective/prospective.svg'
+import newBadge from "./icons/New/new.svg";
 
 import './style.css'
 
@@ -120,7 +121,10 @@ class Landing extends Component {
 							<Link to="/settings" className="button red-shadow" style={{backgroundImage: `url(${settingsIcon})` }}>Settings</Link>
 						</div> : false}
 						<div className="row">
-							<Link to="/student?forwardTo=prospective-student" className="button yellow-shadow" style={{backgroundImage: `url(${prospective})` }}>Prospective</Link>
+							<div className="badge-container">
+								<img className="new-badge" src={newBadge}/>
+								<Link to="/student?forwardTo=prospective-student" className="button yellow-shadow" style={{backgroundImage: `url(${prospective})` }}>Prospective</Link>
+							</div>
 							<div className="button yellow-shadow" onClick={logout} style={{backgroundImage: `url(${switchUserIcon})` }}>Logout</div>
 						</div>
 					</div>
