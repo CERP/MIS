661d3885759c68827d82f509db43d133cf5acc1c
ali ahmad <raoaliahmad03@gmail.com>
2019-03-20 13:31:25 +0500

    seperated  tag filter from student list

--- web/src/components/List/index.js
+++ web/src/components/List/index.js
@@ -9,10 +9,7 @@ class List extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			filterText : "",
-			active: true,
-			inActive: false,
-			tag:""
+			filterText : ""
 		}
 		this.former = new Former(this, [])
 	}
@@ -21,78 +18,12 @@ class List extends Component {
 		this.setState({filterText:e.target.value});
 	}
 
-	uniqueTags = (students) => {
+	render(){
+		const {items, toLabel, Component, children } = this.props;
 
-		const tags = new Set();
-
-		Object.values(students)
-			.forEach(s => {
-				Object.keys(s.tags || {})
-					.forEach(tag => tags.add(tag))
-			})
-
-		return tags;
-	}
-
-	getListFilterCondition = (item) =>{
-
-		//Active is checked and inactive is unchecked
-		if( this.state.active && !this.state.inActive && this.state.tag === "" ){
-			return item.Active
-			//Show only Active
-		}
-
-		//Active is checked and inactive is unchecked
-		if( this.state.active && !this.state.inActive && this.state.tag !== "" ){
-
-			if(item.tags === undefined){
-				return false
-			}
-			return item.Active && Object.keys(item.tags).includes(this.state.tag)
-			//Show active with selected tag
-		}
-
-		//Active is checked and inactive is checked
-		if( this.state.active && this.state.inActive && this.state.tag === "" ){
-			return true
-			//show All
-		}
-		
-		//Active is checked and inactive is checked
-		if( this.state.active && this.state.inActive && this.state.tag !== "" ){
-
-			if(item.tags === undefined){
-				return false
-			}
-			return Object.keys(item.tags).includes(this.state.tag)
-			//show all with selected tag
-		}
-
-		//Active is unchecked and inactive is checked
-		if( !this.state.active && this.state.inActive && this.state.tag === "" ){
-			return !item.Active
-			//show only InActive
-		}
-
-		//Active is unchecked and inactive is checked
-		if( !this.state.active && this.state.inActive && this.state.tag !== "" ){
-
-			if(item.tags === undefined){
-				return false
-			}
-			return !item.Active && Object.keys(item.tags).includes(this.state.tag)
-			//Show InActive with selected tag
-		}
-
-	}
-
-	render() {
-		const {items, toLabel, Component, students } = this.props;
-
-		
 		const filteredList = items
 			.filter(item => {
-				return toLabel(item) !== undefined && toLabel(item).toLowerCase().includes(this.state.filterText.toLowerCase()) && this.getListFilterCondition(item)
+				return toLabel(item) !== undefined && toLabel(item).toLowerCase().includes(this.state.filterText.toLowerCase())
 			})
 			.sort((a,b) => toLabel(b).localeCompare(this.state.filterText) - toLabel(a).localeCompare(this.state.filterText))
 
@@ -107,31 +38,7 @@ class List extends Component {
 			</div>
 			<input className="search-bar no-print" type="text" placeholder="Search" onChange={this.onChange}/>
 
-			{ students && <div className="row filter-container">
-					
-					<div className="row checkbox-container">
-						<div className="checkbox">
-							<input type="checkbox" {...this.former.super_handle(["active"])} style={{height:"20px"}}/>
-								Active
-						</div>
-						<div className="checkbox">
-							<input type="checkbox" {...this.former.super_handle(["inActive"])} style={{height:"20px"}} />
-								InActive
-						</div>
-					</div>
-
-					<select className="list-select" {...this.former.super_handle(["tag"])}>
-						<option value="">Tag</option>
-						{
-							[...this.uniqueTags(students).keys()]
-							.filter(tag => tag !== "PROSPECTIVE")
-							.map(tag => <option key={tag} value={tag}> {tag} </option>)
-						}
-					</select>
-					
-			</div>
-			}
-
+			{ children }
 			<div className="list">
 			{
 				filteredList.map(item => Component(item))
