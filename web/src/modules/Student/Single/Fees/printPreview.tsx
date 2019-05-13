62aaa908c140ae7c37b4904a256209c7e77eead1
ali ahmad <raoaliahmad03@gmail.com>
2019-05-13 15:59:20 +0500

    done with changes

--- web/src/modules/Student/Single/Fees/printPreview.tsx
+++ web/src/modules/Student/Single/Fees/printPreview.tsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
-import moment from 'moment';
-import { PrintHeader } from '../../../../components/Layout'
-import { numberWithCommas } from '../../../Class/Single/ClassFeeMenu'
 import { connect } from 'react-redux'
 import { withRouter, RouteComponentProps } from 'react-router-dom'
 import qs from 'querystring'
 import { getSectionsFromClasses } from '../../../../utils/getSectionsFromClasses';
+import { getFilteredPayments } from '../../../../utils/getFilteredPayments'
+import { StudentLedgerPage } from './StudentLedgerPage';
 
 interface P {
 	classes: RootDBState["classes"]
-	curr_student: MISStudent
+	student: MISStudent
 	faculty_id: RootReducerState["auth"]["faculty_id"]
 	students: RootDBState["students"]
 	settings: RootDBState["settings"]
@@ -34,58 +33,16 @@ class printPreview extends Component <propTypes, S>{
 		}
 	}
 
-	month = () => qs.parse(this.props.location.search).month || ""
-	year = () => qs.parse(this.props.location.search).year || ""
-
-	getFilterCondition = (payment: MISStudentPayment) =>
-	{
-		//when both are empty
-		if(this.month() === "" && this.year() === "") {
-			return true
-		}
-		//when month is empty	
-		if(this.month() === "" && this.year() !== ""){
-			return  moment(payment.date).format("YYYY") === this.year();
-
-		}
-		//when year is empty
-		if(this.month() !== "" && this.year() === ""){
-			return moment(payment.date).format("MMMM") === this.month()
-
-		}
-		//when both are not empty
-		if(this.month() !== "" && this.year() !== "")
-		{
-			return moment(payment.date).format("MMMM") === this.month() && moment(payment.date).format("YYYY") === this.year();
-		}
-	}
-	
+	month = () : string => `${qs.parse(this.props.location.search)["?month"] || ""}`
+	year = () : string => `${qs.parse(this.props.location.search)["year"] || ""}`
 
 	render() {
 
-		const { classes, curr_student, settings } = this.props
+		const { classes, student, settings } = this.props
 
 		const sections =  getSectionsFromClasses(classes)
-
-		let curr_class = ""
-
-		for(let s of sections){
-			if(curr_student.section_id === s.id)
-			{
-				curr_class = s.namespaced_name
-				break
-			}
-		}
-
-		const filteredPayments = Object.entries(curr_student.payments || {})
-				.sort(([, a_payment], [, b_payment]) => a_payment.date - b_payment.date)
-				.filter(([id,payment]) => this.getFilterCondition(payment))
-
-		const owed = filteredPayments.reduce((agg, [,curr]) => agg - (curr.type === "SUBMITTED" || curr.type === "FORGIVEN" ? 1 : -1) * curr.amount, 0)
-		
-		const totalOwed = Object.entries(curr_student.payments || {})
-				.sort(([, a_payment], [, b_payment]) => a_payment.date - b_payment.date)
-				.reduce((agg, [,curr]) => agg - (curr.type === "SUBMITTED" || curr.type === "FORGIVEN" ? 1 : -1) * curr.amount, 0)
+		const curr_class = sections.find(x => x.id === student.section_id ).namespaced_name
+		const filteredPayments = getFilteredPayments(student, this.year(), this.month())
 
 	return	<div className="student-fees-ledger">
 
@@ -93,30 +50,24 @@ class printPreview extends Component <propTypes, S>{
 
  			 	<div className="voucher-row">
 				<StudentLedgerPage 
-						filteredPayments = {filteredPayments} 
-						owed = {owed}
-						totalOwed = {totalOwed} 
-						settings = {settings}
-						curr_student = {curr_student}
-						curr_class = {curr_class}
+					payments = {filteredPayments} 
+					settings = {settings}
+					student = {student}
+					class_name = {curr_class}
 				/>
-				
+
 				<div className="row print-voucher">
 					<StudentLedgerPage 
-						filteredPayments = {filteredPayments} 
-						owed = {owed}
-						totalOwed = {totalOwed} 
+						payments = {filteredPayments} 
 						settings = {settings}
-						curr_student = {curr_student}
-						curr_class = {curr_class}
+						student = {student}
+						class_name = {curr_class}
 					/>
 					<StudentLedgerPage 
-						filteredPayments = {filteredPayments} 
-						owed = {owed}
-						totalOwed = {totalOwed} 
+						payments = {filteredPayments} 
 						settings = {settings}
-						curr_student = {curr_student}
-						curr_class = {curr_class}
+						student = {student}
+						class_name = {curr_class}
 				/>
 
 				</div>
@@ -128,75 +79,6 @@ class printPreview extends Component <propTypes, S>{
 export default connect((state: RootReducerState, { match: { params: { id } } } : { match: { params: { id: string}}}) => ({
 	classes: state.db.classes,
 	faculty_id: state.auth.faculty_id,
-	curr_student: state.db.students[id],
+	student: state.db.students[id],
 	settings: state.db.settings,
 }))(withRouter(printPreview))
-
-
-interface StudentLedgerPageProp {
-	filteredPayments: [string, MISStudentPayment][] 
-	owed: number 
-	totalOwed: number
-	settings: RootDBState["settings"]
-	curr_student: MISStudent
-	curr_class: string
-}
-
-const StudentLedgerPage : React.SFC < StudentLedgerPageProp > = ({ filteredPayments, owed, totalOwed, settings, curr_student, curr_class }) => {
-
-	return <div className="payment-history section print-page" >
-			<PrintHeader settings={settings} logo={""}/>
-
-			<div className="divider">Student Information</div>
-				<div className="row info">
-					<label> Name:</label>
-					<div>{curr_student.Name}</div>
-				</div>
-				<div className="row info">
-					<label> Father Name:</label>
-					<div>{curr_student.ManName}</div>
-				</div>
-				<div className="row info">
-					<label> Class Name:</label>
-					<div>{curr_class}</div>
-				</div>
-				<div className="row info">
-					<label> Roll #:</label>
-					<div>{curr_student.RollNumber}</div>
-				</div>
-				<div className="row info">
-					<label> Adm # :</label>
-					<div>{curr_student.RollNumber}</div>
-				</div>
-				<div className="row info">
-					<label style={{ color: owed <= 0 ? "#5ECDB9" : "#FC6171" }}> {owed <= 0 ? "Total Advance:" : "Total Pending:"}</label>
-					<div style={{ color: owed <= 0 ? "#5ECDB9" : "#FC6171" }}>{`${numberWithCommas(Math.abs(totalOwed))} Rs`}</div>
-				</div>
-
-			<div className="divider">Payment Information</div>
-
-			<div className="table row heading">
-				<label><b>Date</b></label>
-				<label><b>Label</b></label>
-				<label><b>Amount</b></label>
-			</div>
-
-			{filteredPayments
-				.map(([id, payment]) => {
-
-					return <div className="payment" key={id} >
-						<div className="table row">
-							<div>{moment(payment.date).format("DD/MM")}</div>
-							<div>{payment.type === "SUBMITTED" ? "Payed" : payment.type === "FORGIVEN" ? "Need Scholarship" : payment.fee_name || "Fee"}</div>
-							<div>{payment.type === "OWED" ? `${numberWithCommas(payment.amount)}` : `${numberWithCommas(payment.amount)}`}</div>
-						</div>
-					</div>
-				}
-			)}
-
-			<div className="table row last">
-				<label style={{ color: owed <= 0 ? "#5ECDB9" : "#FC6171" }}><b>{owed <= 0 ? "Advance:" : "Pending:"}</b></label>
-				<div style={{ color: owed <= 0 ? "#5ECDB9" : "#FC6171" }}><b>{numberWithCommas( Math.abs(owed))}</b></div>
-			</div>
-	</div>
-}
