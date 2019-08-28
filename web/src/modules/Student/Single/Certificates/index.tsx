7206815cc8cf73ba8a5bb1455494dc923a34c183
Mudassar Ali <Mudassaralics007@gmail.com>
2019-08-28 15:34:56 +0500

    2 lines add in school leaving certificate

--- web/src/modules/Student/Single/Certificates/index.tsx
+++ web/src/modules/Student/Single/Certificates/index.tsx
@@ -246,6 +246,13 @@ const SchoolLeavingCertificate: React.FC <CertificateProps> = ({ curr_student, r
 				<div>	
 				</div>
 			</div>
+
+			<div className="cert-row">
+				<label>&nbsp;</label>
+				<div>	
+				</div>
+			</div>
+
 		</div>
 
 		<div className="footer">
