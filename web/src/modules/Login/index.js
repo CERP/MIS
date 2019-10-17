2e78d039bcfcdf75bd293a8482ff7a5666f4cfe1
Mudassar045 <mudassaralics007@gmail.com>
2019-10-17 13:12:45 +0500

    revert input type

--- web/src/modules/Login/index.js
+++ web/src/modules/Login/index.js
@@ -107,7 +107,7 @@ class Login extends Component {
 					</div>
 					<div className="row">
 						<label>Password</label>
-						<input type="password" {...this.former.super_handle(["password"])} placeholder="Password" autoCapitalize="off" onKeyDown={this.handleKeyDown}/>
+						<input type="text" {...this.former.super_handle(["password"])} placeholder="Password" autoCapitalize="off" onKeyDown={this.handleKeyDown}/>
 					</div>
 					<div className="button save" onClick={this.onLogin}>Login</div>
 				</div>
