ae20f2922a899da9b5816d8a811a93970ec14c0a
Mudassar045 <mudassaralics007@gmail.com>
2019-11-08 15:43:03 +0500

    add description and clean the code

--- web/src/utils/chunkify.ts
+++ web/src/utils/chunkify.ts
@@ -1,18 +1,29 @@
-export const chunkify = (kvArray: any, chunkSize: number = 10) => {
+/*
+ *  description: takes a single array, breaks it into chunks of size chunkSize
+ *               and returns an array of arrays of max length chunkSize
+ * 
+ *  params: kvArray, chunkSize
+ *  defaults: kvArray = [], chunkSize = 32
+ *  return: [[], [], [], ...]
+ */
+
+export const chunkify = (kvArray: any, chunkSize: number = 32) => {
     if(kvArray.length === 0) {
         return [[]]
-    } else if(kvArray.length <= chunkSize) {
+    } 
+    
+    if(kvArray.length <= chunkSize) {
         return [kvArray]
-    } else {
-        return kvArray.reduce((agg: any, curr: any, index: number) => {
-            const chunkIndex = Math.floor(index / chunkSize)
-            if(!agg[chunkIndex]) {
-              agg[chunkIndex] = [] // create a new chunk
-            }
-            agg[chunkIndex].push(curr)
-            return agg
-        }, [])
     }
+
+    return kvArray.reduce((agg: any, curr: any, index: number) => {
+        const chunkIndex = Math.floor(index / chunkSize)
+        if(!agg[chunkIndex]) {
+            agg[chunkIndex] = [] // create a new chunk
+        }
+        agg[chunkIndex].push(curr)
+        return agg
+    }, [])
 }
 
 export default chunkify
