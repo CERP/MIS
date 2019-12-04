6bc804e61004603c1c057776de495350bf7255cc
Mudassar045 <mudassaralics007@gmail.com>
2019-12-04 14:22:03 +0500

    initial fee voucher redesign

--- web/src/modules/Settings/HistoricalFees/historical-fee.tsx
+++ web/src/modules/Settings/HistoricalFees/historical-fee.tsx
@@ -255,6 +255,7 @@ class historicalFee extends Component <propTypes, S > {
 					student = {selected_student}
 					class_name = {curr_class_name}
 					voucherNo = {777}
+					css_style = {""}
 				/>
 			</div>}
 
