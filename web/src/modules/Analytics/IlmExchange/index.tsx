89f4f8467f07fe53137b96d1cee271a06515fd19
Mudassar045 <mudassaralics007@gmail.com>
2020-05-31 20:46:49 +0500

    minor changes

--- web/src/modules/Analytics/IlmExchange/index.tsx
+++ web/src/modules/Analytics/IlmExchange/index.tsx
@@ -22,12 +22,12 @@ type ReduceLessonMap = {
 
 function computeVideosData() {
 
-	let agg: ReduceLessonMap = {}
-
 	const lessons_meta = data["lessons"]
 
+	let agg: ReduceLessonMap = {}
+
 	Object.entries(data["events"])
-		.forEach(([device_id, lessons]) => {
+		.forEach(([_, lessons]) => {
 
 			for (const item of Object.values(lessons)) {
 
@@ -53,24 +53,36 @@ function computeVideosData() {
 	return agg
 }
 
+const getSortedEntries = (videos_data: ReduceLessonMap) => {
+	return Object.entries(videos_data)
+		.sort(([, a], [_, b]) => b.count - a.count)
+}
+
+const getMostWatchCount = (sorted_entries: any) => {
+	const [_, lesson_meta] = sorted_entries[0]
+	return lesson_meta ? lesson_meta.count : 0
+}
 
 const IlmExchangeAnalytics: React.FC<PropsType> = ({ }) => {
 
 	const computed_videos_data: ReduceLessonMap = computeVideosData()
+
+	const sorted_entries = getSortedEntries(computed_videos_data)
+	const most_watch_count = getMostWatchCount(sorted_entries)
 
 	return (
 		<div className="section-container">
 			<div className="divider">IlmExchange Analytics</div>
 			<div className="text-left" style={{ marginTop: 4, marginBottom: 4, fontSize: "1.25rem" }}>Most viewed videos</div>
 			<div className="section">
-				<table>
+				<table style={{ width: "100%" }}>
 					<tbody>
 						{
-							Object.entries(computed_videos_data)
-								.sort(([, a], [_, b]) => b.count - a.count)
+							sorted_entries
 								.map(([lesson_id, lesson_meta]) => {
 									return <tr key={lesson_id}>
 										<td>{lesson_meta.title}</td>
+										<td><progress max={most_watch_count} value={lesson_meta.count} /></td>
 										<td>{lesson_meta.count} Views</td>
 									</tr>
 								})
