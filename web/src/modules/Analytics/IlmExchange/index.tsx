2798e228a29f78ebf883cedc024ad260a802be23
Mudassar045 <mudassaralics007@gmail.com>
2020-06-27 11:51:08 +0500

    add class, subject and date filter

--- web/src/modules/Analytics/IlmExchange/index.tsx
+++ web/src/modules/Analytics/IlmExchange/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useMemo } from "react"
 import { connect } from 'react-redux'
 
 import { fetchLessons } from 'actions/core'
@@ -8,7 +8,9 @@ import LessonViewerModal from './lessonViewer'
 import { PlayIcon } from 'assets/icons'
 import SortAscendingIcon from "assets/svgs/react/SortAscending"
 import SortDescendingIcon from "assets/svgs/react/SortDescending"
+import CalendarIcon from "assets/svgs/react/Calendar"
 import IlmxGraphs from "./Graphs"
+import moment from 'moment'
 
 import './style.css'
 
@@ -30,14 +32,25 @@ interface S {
 
 const IlmExchangeAnalytics: React.FC<PropsType> = ({ students, events, lessons, fetchLessons, classes }) => {
 
+	const current_date = moment.now()
+
 	const [toggleSortOrder, setToggleSortOrder] = useState(false)
+	const [toggleCalendar, setToggleCalendar] = useState(false)
+	const [classFilter, setClassFilter] = useState('')
+	const [subjectFilter, setSubjectFilter] = useState('')
+	const [dateFilter, setDateFilter] = useState(null)
 
 	useEffect(() => {
 		fetchLessons()
 	}, [fetchLessons])
 
-	const computed_lessons_data: AugmentedIlmxLessons = computeLessonsData(events, lessons)
-	const sorted_entries = getSortedEntries(computed_lessons_data, toggleSortOrder)
+	const { classTitles, subjects } = useMemo(() => getClassSubjectsInfo(events), [events])
+	const lessons_data = useMemo(
+		() => computeLessonsData(events, lessons, dateFilter, classFilter, subjectFilter),
+		[events, lessons, dateFilter, classFilter, subjectFilter]
+	)
+
+	const sorted_entries = getSortedEntries(lessons_data, toggleSortOrder)
 
 	const [stateProps, setStateProps] = useState<S>({
 		showViewerModal: false,
@@ -46,9 +59,7 @@ const IlmExchangeAnalytics: React.FC<PropsType> = ({ students, events, lessons, 
 	})
 
 	const handleClickShowViewers = (lesson_id: string) => {
-
 		const scrollY = window.pageYOffset
-
 		setStateProps({
 			...stateProps,
 			showViewerModal: !stateProps.showViewerModal,
@@ -70,18 +81,70 @@ const IlmExchangeAnalytics: React.FC<PropsType> = ({ students, events, lessons, 
 		window.scrollTo(0, stateProps.scrollY)
 	}
 
+	const hanleToggleCalender = () => {
+		if (!toggleCalendar) {
+			setDateFilter(current_date)
+		} else {
+			setDateFilter(null)
+		}
+
+		setToggleCalendar(!toggleCalendar)
+	}
+
+	const handleDateChange = (input_date: string) => {
+		const date = moment(input_date, "YYYY-MM-DD").unix() * 1000
+		setDateFilter(date)
+	}
+
 	return (
 		<div className="ilmx-analytics section-container">
 			<div className="divider">IlmExchange Analytics</div>
-			<div className="text-left" style={{ marginTop: 4, fontWeight: 700, marginBottom: "1rem", fontSize: "1.25rem" }}>Last week activity</div>
+			<div className="text-left heading" >Last week activity</div>
 			<div className="section">
 				<IlmxGraphs events={events} />
 			</div>
-			<div className="text-left" style={{ marginTop: 4, fontWeight: 700, marginBottom: "1rem", fontSize: "1.25rem" }}>Most viewed videos</div>
+			<div className="text-left heading" style={{ marginTop: 4, fontWeight: 700, marginBottom: "1rem", fontSize: "1.25rem" }}>Most viewed videos</div>
 			<div className="section">
-				<div className="most-viewed-videos sort" onClick={() => setToggleSortOrder(!toggleSortOrder)} title="Sort Videos">
-					{toggleSortOrder ? <SortAscendingIcon /> : <SortDescendingIcon />}
+				<div className="row video-filter">
+					<select onChange={(e) => setClassFilter(e.target.value)}>
+						<option value="">Select Class</option>
+						{
+							classTitles
+								.sort()
+								.reverse()
+								.sort((a, b) => {
+									if (parseInt(a) && parseInt(b)) return +a - +b
+									if (parseInt(a) && !parseInt(b)) return 1
+									if (parseInt(b) && !parseInt(b)) return -1
+									return 0
+								})
+								.map(c_title => <option key={c_title} value={c_title}>Class {c_title}</option>)
+						}
+					</select>
+					<select onChange={(e) => setSubjectFilter(e.target.value)}>
+						<option value="">Select Subject</option>
+						{
+							subjects
+								.sort()
+								.map(subject => <option key={subject}>{subject}</option>)
+						}
+					</select>
+					<div className="toggle-actions-container">
+						<div className="toggle-actions" onClick={() => setToggleSortOrder(!toggleSortOrder)} title="Sort Videos">
+							{toggleSortOrder ? <SortAscendingIcon /> : <SortDescendingIcon />}
+						</div>
+						<div className="toggle-actions" onClick={hanleToggleCalender} title="Date">
+							<CalendarIcon />
+						</div>
+					</div>
 				</div>
+				{toggleCalendar && <div className="calender">
+					<input type="date" name="lesson_videos"
+						onChange={(e) => handleDateChange(e.target.value)}
+						defaultValue={moment(dateFilter).format("YYYY-MM-DD")}
+						max={moment(current_date).format("YYYY-MM-DD")} />
+				</div>
+				}
 				<div className="container">
 					{
 						sorted_entries
@@ -97,7 +160,7 @@ const IlmExchangeAnalytics: React.FC<PropsType> = ({ students, events, lessons, 
 										</div>
 									</div>
 									<div className="card-row">
-										<p className="student-class-title">Class: {getClassTitleFromLessonId(lesson_id)}-{getBookTitleFromLessonId(lesson_id)}</p>
+										<p className="student-class-title">Class: {getClassTitleFromLessonId(lesson_id)}-{getSubjectTitleFromLessonId(lesson_id)}</p>
 									</div>
 									<div className="card-row">
 										<div className="more-detail">
@@ -111,10 +174,10 @@ const IlmExchangeAnalytics: React.FC<PropsType> = ({ students, events, lessons, 
 							))
 					}
 					{
-						stateProps.showViewerModal && computed_lessons_data && <Modal>
+						stateProps.showViewerModal && lessons_data && <Modal>
 							<LessonViewerModal
 								lessonId={stateProps.lessonId}
-								lessons={computed_lessons_data}
+								lessons={lessons_data}
 								students={students}
 								classes={classes}
 								onClose={handleToggleModal} />
@@ -142,7 +205,11 @@ type AugmentedIlmxLessons = {
 	[id: string]: AugmentedIlmxLesson
 }
 
-function computeLessonsData(events: PropsType["events"], lessons: PropsType["lessons"]) {
+type ComputeLessonsData = {
+	(events: PropsType["events"], lessons: PropsType["lessons"], date: number, class_title: string, subject: string): AugmentedIlmxLessons
+}
+
+const computeLessonsData: ComputeLessonsData = (events, lessons, date, class_title, subject) => {
 
 	let agg: AugmentedIlmxLessons = {}
 
@@ -153,37 +220,46 @@ function computeLessonsData(events: PropsType["events"], lessons: PropsType["les
 	}
 
 	Object.entries(events || {})
-		.forEach(([_, lessons_history]) => {
+		.forEach(([, lessons_history]) => {
 
-			for (const item of Object.values(lessons_history)) {
-				if (item.type === "VIDEO") {
+			for (const [timestamp, item] of Object.entries(lessons_history)) {
+
+				const is_same_date = date ? moment(parseInt(timestamp)).isSame(moment(date), "day") : true
+
+				if (item.type === "VIDEO" && is_same_date) {
 
 					const { lesson_id, duration, student_id } = item
 
-					if (agg[lesson_id]) {
+					const s_title = getSubjectTitleFromLessonId(lesson_id)
+					const c_title = getClassTitleFromLessonId(lesson_id)
 
-						agg[lesson_id] = {
-							...agg[lesson_id],
-							watchCount: agg[lesson_id].watchCount + 1,
-							watchTime: agg[lesson_id].watchTime + duration,
-							viewers: {
-								...agg[lesson_id].viewers,
-								[student_id]: {
-									watchCount: agg[lesson_id].viewers[student_id] ? agg[lesson_id].viewers[student_id].watchCount + 1 : 1,
-									watchTime: agg[lesson_id].viewers[student_id] ? agg[lesson_id].viewers[student_id].watchTime + duration : duration
+					if ((subject ? s_title === subject : true) && (class_title ? c_title === class_title : true)) {
+
+						if (agg[lesson_id]) {
+
+							agg[lesson_id] = {
+								...agg[lesson_id],
+								watchCount: agg[lesson_id].watchCount + 1,
+								watchTime: agg[lesson_id].watchTime + duration,
+								viewers: {
+									...agg[lesson_id].viewers,
+									[student_id]: {
+										watchCount: agg[lesson_id].viewers[student_id] ? agg[lesson_id].viewers[student_id].watchCount + 1 : 1,
+										watchTime: agg[lesson_id].viewers[student_id] ? agg[lesson_id].viewers[student_id].watchTime + duration : duration
+									}
 								}
 							}
-						}
-					} else {
-						agg[lesson_id] = {
-							watchCount: 1,
-							watchTime: duration,
-							// @ts-ignore
-							...lessons_meta[lesson_id],
-							viewers: {
-								[student_id]: {
-									watchCount: 1,
-									watchTime: duration
+						} else {
+							agg[lesson_id] = {
+								watchCount: 1,
+								watchTime: duration,
+								// @ts-ignore
+								...lessons_meta[lesson_id],
+								viewers: {
+									[student_id]: {
+										watchCount: 1,
+										watchTime: duration
+									}
 								}
 							}
 						}
@@ -193,6 +269,26 @@ function computeLessonsData(events: PropsType["events"], lessons: PropsType["les
 		})
 
 	return agg
+}
+
+const getClassSubjectsInfo = (events: PropsType["events"]) => {
+
+	let class_titles = new Set<string>()
+	let subjects = new Set<string>()
+	Object.entries(events || {})
+		.forEach(([, lessons_history]) => {
+			for (const [, item] of Object.entries(lessons_history)) {
+				if (item.type === "VIDEO") {
+					const { lesson_id } = item
+					const class_title = getClassTitleFromLessonId(lesson_id)
+					const subject = getSubjectTitleFromLessonId(lesson_id)
+					class_titles.add(class_title)
+					subjects.add(subject)
+				}
+			}
+		})
+
+	return { classTitles: [...class_titles], subjects: [...subjects] }
 }
 
 const getSortedEntries = (lessons_data: AugmentedIlmxLessons, sortOrder: boolean) => {
@@ -209,6 +305,7 @@ const getSortedEntries = (lessons_data: AugmentedIlmxLessons, sortOrder: boolean
 const getClassTitleFromLessonId = (lessonId: string): string => {
 	return lessonId.split("-")[1] || "nil"
 }
-const getBookTitleFromLessonId = (lessonId: string): string => {
+
+const getSubjectTitleFromLessonId = (lessonId: string): string => {
 	return lessonId.split("-")[2] || "nil"
 }
