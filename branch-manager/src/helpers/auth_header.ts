8a2fd0a01398473628a9ae48b5fd8a39f2b86a65
mudassar045 <mudassaralics007@gmail.com>
2020-09-21 00:21:24 +0500

    simplify auth in header

--- branch-manager/src/helpers/auth_header.ts
+++ branch-manager/src/helpers/auth_header.ts
@@ -3,7 +3,7 @@ export const auth_header = () => {
     const auth = JSON.parse(localStorage.getItem('auth') || '')
 
     if (auth && auth.token) {
-        return { 'Authorization': 'Basic ' + auth.token }
+        return { 'authorization': auth.token }
     }
 
     return {}
