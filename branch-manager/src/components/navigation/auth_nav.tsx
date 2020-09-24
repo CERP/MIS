023053977d28d85845a3a3f9069a71b4c2ecd829
mudassar045 <mudassaralics007@gmail.com>
2020-09-24 14:09:20 +0500

    make navbar responsive

--- branch-manager/src/components/navigation/auth_nav.tsx
+++ branch-manager/src/components/navigation/auth_nav.tsx
@@ -3,33 +3,34 @@ import { Link } from 'react-router-dom'
 
 const AuthNavigation = () => {
 
-	const [menu, setMenu] = useState(true)
+	const [menu, setMenu] = useState(false)
 
 	return (
-		<nav className="w-full z-30 py-1 bg-white shadow border-b border-red-500">
-			<div className="w-full flex items-center justify-between mt-0 px-6 py-2">
-				<label className="cursor-pointer md:hidden block" onClick={() => setMenu(!menu)}>
-					<svg className="fill-current text-red-500" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
-						<title>menu</title>
-						<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-					</svg>
-				</label>
-				<div className="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1">
-					<nav>
-						<ul className="md:flex items-center justify-between text-base text-red-500 pt-4 md:pt-0">
-							<li><Link to="/home" className="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2">Home</Link></li>
-							<li><Link to="/analytics" className="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2">Analytics</Link></li>
-						</ul>
-					</nav>
+		<nav className="flex items-center justify-between flex-wrap bg-white py-4 md:px-12 shadow border-b border-red-500">
+			<div className="flex justify-between md:w-auto w-full md:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 md:pb-0">
+				<div className="flex items-center flex-shrink-0 text-red-500 mr-16">
+					<Link to="/home" className="font-semibold text-lg tracking-tight">MIS Branch Manager</Link>
 				</div>
-
-				<div className="order-2 md:order-3 flex flex-wrap items-center justify-end mr-0 md:mr-4">
-					<div className="auth flex items-center w-full md:w-full">
-						<Link to="/login" className="bg-transparent text-red-500 px-4 py-1 rounded border border-red-500 mr-4 hover:bg-red-400 hover:text-white">Logout</Link>
-					</div>
+				<div className="block md:hidden ">
+					<button className="flex items-center px-3 py-2" onClick={() => setMenu(!menu)}>
+						<svg className="fill-current text-red-500" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+							<title>menu</title>
+							<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+						</svg>
+					</button>
 				</div>
 			</div>
-		</nav >
+
+			<div className={`${menu ? '' : 'hidden'} w-full flex-grow md:flex md:items-center md:w-auto md:px-3 px-8`}>
+				<div className="text-md font-bold text-red-500 md:flex-grow">
+					<Link to="/analytics" className=" block mt-4 md:inline-block md:mt-0 px-4 py-2 rounded mr-2"> Analytics </Link>
+				</div>
+				<div className="flex ">
+					<Link to="/login" className=" block text-md px-3  ml-2 py-1 text-red-500 font-bold mt-4 md:mt-0 md:rounded md:border md:border-red-500 md:hover:bg-red-400 md:hover:text-white">Logout</Link>
+				</div>
+			</div>
+
+		</nav>
 	)
 }
 
--- branch-manager/src/pages/login/index.tsx
+++ branch-manager/src/pages/login/index.tsx
@@ -66,22 +66,22 @@ export const Login: React.FC<P> = () => {
 					<div className="mb-4">
 						<label className="sr-only">Username</label>
 						<input className="border-solid border border-gray-400 rounded px-2 py-2" type="text"
-							name="username" placeholder="usernmae" onChange={(e) => handle_change(e)} required />
+							name="username" placeholder="usernmae" autoCapitalize="off" autoCorrect="off" autoComplete="off" onChange={(e) => handle_change(e)} required />
 					</div>
 					<div className="mb-4">
 						<label className="sr-only">Password</label>
 						<div className="relative">
 							<input className="border-solid border border-gray-400 rounded px-2 py-2" type={`${toggle ? 'text' : 'password'}`}
-								name="password" placeholder="Password" onChange={(e) => handle_change(e)} required />
+								name="password" placeholder="Password" autoCapitalize="off" autoCorrect="off" autoComplete="off" onChange={(e) => handle_change(e)} required />
 							<div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5 cursor-pointer">
 								{toggle ? <HidePassword onHide={handle_show_hide} /> : <ShowPassword onShow={handle_show_hide} />}
 							</div>
 						</div>
 					</div>
-					<button className={`inline-flex items-center justify-center px-4 py-2 border text-base font-medium leading-6 text-center 
+					<button className={`inline-flex px-4 py-2 border text-base font-medium leading-6 text-center 
 										rounded text-white bg-red-600 hover:bg-red-500 w-full ${logging ? 'cursor-not-allowed' : ''}`}>
 						{logging && <Spinner />}
-						Login
+						<span className="mx-auto">Login</span>
 					</button>
 					<p className="text-sm my-2 text-red-700">{alert && alert.message}</p>
 				</div>
