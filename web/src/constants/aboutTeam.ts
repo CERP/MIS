caf2a9c1f0fdf57f2e6030c03c7052cea7597334
mudassar045 <mudassaralics007@gmail.com>
2020-10-28 01:20:06 +0500

    change team info

--- web/src/constants/aboutTeam.ts
+++ web/src/constants/aboutTeam.ts
@@ -8,9 +8,34 @@ interface Team {
 
 const TeamMembers: Array<Team> = [
 	{
+		designation: "Principal Investigator",
+		avatar_url: "https://storage.googleapis.com/ilmx-product-images/asimk-min.jpg",
+		name: "Asim Khwaja"
+	},
+	{
+		designation: "LEAPS Director",
+		avatar_url: "https://storage.googleapis.com/ilmx-product-images/aboutzainab.jpg",
+		name: "Zainab Qureshi"
+	},
+	{
+		designation: "Project Manager",
+		avatar_url: "https://storage.googleapis.com/ilmx-product-images/aboutrooh.jpg",
+		name: "Roohullah Gulzari"
+	},
+	{
 		name: "Taimur Shah",
 		avatar_url: "images/taimur.jpg",
 		designation: "Technology Lead"
+	},
+	{
+		designation: "Research Assistant",
+		avatar_url: "https://storage.googleapis.com/ilmx-product-images/aboutabsar.jpg",
+		name: "Absar Ali"
+	},
+	{
+		designation: "Research Assistant",
+		avatar_url: "https://storage.googleapis.com/ilmx-product-images/aboutfarah.jpeg",
+		name: "Farah Basit"
 	},
 	{
 		name: "Ayesha Ahmed",
@@ -18,41 +43,68 @@ const TeamMembers: Array<Team> = [
 		designation: "Research Assistant"
 	},
 	{
-		name: "Ali Ahmad",
-		avatar_url: "/images/ali_ahmad.jpg",
-		designation: "Senior Developer"
+		designation: "Data Research Assistant",
+		avatar_url: "https://storage.googleapis.com/ilmx-product-images/yusuf.jpeg",
+		name: "Yusuf Khwaja"
 	},
 	{
-		name: "Mudassar Ali",
-		avatar_url: "/images/mudassar.jpg",
-		designation: "Developer",
+		designation: "Field Manager",
+		avatar_url: "https://storage.googleapis.com/ilmx-product-images/aboutumer.jpg",
+		name: "Umer Farooq"
 	},
 	{
 		name: "Bisma Hafeez",
 		avatar_url: "/images/bisma.jpg",
 		designation: "Call Center Assistant",
-		phone: "+923481112004"
+		// phone: "+923481112004"
 	},
+	{
+		avatar_url: "https://storage.googleapis.com/ilmx-product-images/aboutali.jpg",
+		designation: "Field Associate",
+		name: "Ali Husnain"
+	},
+	{
+		avatar_url: "https://storage.googleapis.com/ilmx-product-images/aboutasim.jpg",
+		designation: "Field Associate",
+		name: "Asim Zaheer"
+	},
+	{
+		avatar_url: "https://storage.googleapis.com/ilmx-product-images/aboutkaleem.jpg",
+		designation: "Field Associate",
+		name: "Kaleem Majeed"
+	},
+
+	// {
+	// 	name: "Ali Ahmad",
+	// 	avatar_url: "/images/ali_ahmad.jpg",
+	// 	designation: "Senior Developer"
+	// },
+	// {
+	// 	name: "Mudassar Ali",
+	// 	avatar_url: "/images/mudassar.jpg",
+	// 	designation: "Developer",
+	// },
+
 	{
 		name: "Farooq Azhar",
 		avatar_url: "/images/farooq_azhar.jpg",
-		designation: "Area Manager",
-		district: "Sialkot",
-		phone: "+923410924945"
+		designation: "Field Associate",
+		// district: "Sialkot",
+		// phone: "+923410924945"
 	},
 	{
 		name: "Zahid Riaz",
 		avatar_url: "/images/zahid.jpg",
-		designation: "Area Manager",
-		district: "Gujranwala",
-		phone: "+923460089862"
+		designation: "Field Associate",
+		// district: "Gujranwala",
+		// phone: "+923460089862"
 	},
 	{
 		name: "Ali Zohaib",
 		avatar_url: "/images/ali_zohaib.jpg",
-		designation: "Area Manager",
-		district: "Lahore, Kasur & Sheikhupura",
-		phone: "+923410924944"
+		designation: "Field Associate",
+		// district: "Lahore, Kasur & Sheikhupura",
+		// phone: "+923410924944"
 	}
 ]
 
