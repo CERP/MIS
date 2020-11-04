3ab7724d3e930787694ba0867ae8ad9c4f194934
mudassar045 <mudassaralics007@gmail.com>
2020-11-04 15:29:07 +0500

    minor tweaks

--- web/src/modules/Front/SignUp.js
+++ web/src/modules/Front/SignUp.js
@@ -20,7 +20,7 @@ function emptyProfile() {
 		schoolName: "",
 		schoolPassword: "",
 		packageName: "Free-Trial",
-		typeOfLogin: "",			
+		typeOfLogin: "",
 		referralSchoolName: "",
 		ownerEasypaisaNumber: ""
 	}
@@ -46,15 +46,16 @@ class SignUp extends Component {
 	}
 
 	onSave = () => {
-		let compulsoryFields = this.state.profile.typeOfLogin === 'SCHOOL_REFERRAL' ?
+		const compulsoryFields = this.state.profile.typeOfLogin === 'SCHOOL_REFERRAL' ?
 			checkCompulsoryFields(this.state.profile, [
 				["name"], ["phone"], ['referralSchoolName']
-			]):
+			]) :
 			checkCompulsoryFields(this.state.profile, [
 				["name"], ["phone"]
-			]);
+			])
+
 		if (compulsoryFields) {
-			const errorText = "Please Fill " + compulsoryFields + " !!!";
+			const errorText = "Please Fill " + compulsoryFields + " !";
 
 			return this.setState({
 				banner: {
@@ -74,11 +75,12 @@ class SignUp extends Component {
 	}
 
 	checkNumberExistReason = (reason) => {
-		if(!reason) {
-			return "Please contact at our helpline"
+		if (reason) {
+			return reason.includes(this.state.profile.phone) ?
+				this.state.profile.phone + " already exist. Please login or contact us at our helpline number" : reason
 		}
-		return reason.includes(this.state.profile.phone) ?
-			this.state.profile.phone + " already exist. Please login or contact at our helpline" : reason
+
+		return "Please contact us at out helpline number"
 	}
 
 	UNSAFE_componentWillReceiveProps(props) {
@@ -137,84 +139,84 @@ class SignUp extends Component {
 
 	render() {
 
-	const Span = () => <span style={{ color: "red" }}>*</span>
-
-		if (this.state.redirect){
-			return <Redirect to="/school-login" />	
+		if (this.state.redirect) {
+			return <Redirect to="/school-login" />
 		}
-		return	<div className="section-container section card sign-up">
-				{ this.state.banner.active ? <Banner isGood={this.state.banner.good} text={this.state.banner.text} /> : false}
-				<div className="row">
-					<label> Name </label>
-					<input type="text" {...this.former.super_handle(["profile", "name"])} placeholder='Enter full-name'></input>
-				</div>
-				<div className="row">
-					<label> Mobile # (School ID)</label>
-					<input type="text" {...this.former.super_handle(["profile", "phone"])} placeholder='Enter mobile # e.g 0301xxxxxxx'></input>
-				</div>
-				<div className="row">
-					<label> City/District </label>
-					<select {...this.former.super_handle(["profile", "city"])}>
-						<option value="">Select District</option>
-						{
-							getDistricts().sort().map((dist)=> {
-								return <option key={dist} value={dist}>{toTitleCase(dist)}</option>
-							})
-						}
-					</select>
-				</div>
-				<div className="row">
-					<label> School Name </label>
-					<input type="text" {...this.former.super_handle(["profile", "schoolName"])} placeholder='Enter school-name'></input>
-				</div>
-				<div className="row">
-					<label> School Password </label>
-					<input type="password" {...this.former.super_handle(["profile", "schoolPassword"])} placeholder='Enter password'></input>
-				</div>
-				<div className="row">
-					<label>How did you hear about MISchool?</label>
-					<select {...this.former.super_handle(["profile", "typeOfLogin"])}>
-						<option value="">Select Strategy</option>
-						{
-							[...getStrategies()]
-								.sort()
-								.map(strategy => {
-									const str = strategy.replace(' ', "_").toUpperCase()
-									return <option key={str} value={str}>{strategy}</option>
-								})
-						}
-					</select>
-				</div>
-				{ this.state.profile.typeOfLogin === 'OTHER' &&
-					<div className="row">
-						<label> Other </label>
-						<input type="text" {...this.former.super_handle(["otherLogin"])}></input>
-					</div>
-				}
-				<div className="row">
-					<label> Select Package </label>
-					<select style={{ marginTop: 5 }} {...this.former.super_handle(["profile", "packageName"])}>
-						<option value="Free-Trial">Free-Trial</option>
-					</select>
-				</div>
-					{this.state.profile.typeOfLogin === 'SCHOOL_REFERRAL' && <>
-						<div className="divider"> Referral School Information </div>
-						<div className="row">
-							<label>School Name <Span /></label>
-							<input list="schl-list" {...this.former.super_handle(["profile", "referralSchoolName"])} placeholder="school name" />
-						</div>
-
-						<div className="row">
-							<label>Owner Easy Paisa <Span /></label>
-							<input type="number" {...this.former.super_handle(["profile", "ownerEasypaisaNumber"])} placeholder="Easy Paisa" />
-						</div>
-					</>}				
-				<div className="button red" onClick={() => this.onSave()}> Create Signup</div>
+		return <div className="section-container section card sign-up">
+			{this.state.banner.active ? <Banner isGood={this.state.banner.good} text={this.state.banner.text} /> : false}
+			<div className="row">
+				<label> Name<Span /> </label>
+				<input type="text" {...this.former.super_handle(["profile", "name"])} placeholder='Enter full-name'></input>
 			</div>
-		}
+			<div className="row">
+				<label> Mobile # (School Id)<Span /></label>
+				<input type="text" {...this.former.super_handle(["profile", "phone"])} placeholder='Enter mobile # e.g 0301xxxxxxx'></input>
+			</div>
+			<div className="row">
+				<label> City/District<Span /></label>
+				<select {...this.former.super_handle(["profile", "city"])}>
+					<option value="">Select District</option>
+					{
+						getDistricts().sort().map((dist) => {
+							return <option key={dist} value={dist}>{toTitleCase(dist)}</option>
+						})
+					}
+				</select>
+			</div>
+			<div className="row">
+				<label> School Name<Span /></label>
+				<input type="text" {...this.former.super_handle(["profile", "schoolName"])} placeholder='Enter school-name'></input>
+			</div>
+			<div className="row">
+				<label> School Password<Span /></label>
+				<input type="password" {...this.former.super_handle(["profile", "schoolPassword"])} placeholder='Enter password'></input>
+			</div>
+			<div className="row">
+				<label>How did you hear about MISchool?</label>
+				<select {...this.former.super_handle(["profile", "typeOfLogin"])}>
+					<option value="">Select Strategy</option>
+					{
+						[...getStrategies()]
+							.sort()
+							.map(strategy => {
+								const str = strategy.replace(' ', "_").toUpperCase()
+								return <option key={str} value={str}>{strategy}</option>
+							})
+					}
+				</select>
+			</div>
+			{this.state.profile.typeOfLogin === 'OTHER' &&
+				<div className="row">
+					<label> Other </label>
+					<input type="text" {...this.former.super_handle(["otherLogin"])} placeholder="e.g. internet"></input>
+				</div>
+			}
+			<div className="row">
+				<label> Select Package </label>
+				<select style={{ marginTop: 5 }} {...this.former.super_handle(["profile", "packageName"])}>
+					<option value="Free-Trial">Free-Trial</option>
+				</select>
+			</div>
+			{this.state.profile.typeOfLogin === 'SCHOOL_REFERRAL' && <>
+				<div className="divider"> Referral School Information </div>
+				<div className="row">
+					<label>School Name<Span /></label>
+					<input list="schl-list" {...this.former.super_handle(["profile", "referralSchoolName"])} placeholder="school name" />
+				</div>
+
+				<div className="row">
+					<label>Owner Easy Paisa<Span /></label>
+					<input type="number" {...this.former.super_handle(["profile", "ownerEasypaisaNumber"])} placeholder="Easy Paisa" />
+				</div>
+			</>}
+			<div className="button red" onClick={() => this.onSave()}> Create Signup</div>
+		</div>
+	}
 }
 export default connect(state => ({
 	sign_up_form: state.sign_up_form
 }), dispatch => ({
 	createSignUp: (profile) => dispatch(createSignUp(profile)),
-}))(SignUp);
+}))(SignUp)
+
+const Span = () => <span style={{ color: "var(mis-primary)" }}>*</span>
