022400dded402e2f103c0c3040cd92b49cbc1ed4
Humna Aleem <humna.aleem@cerp.org.pk>
2020-12-29 13:26:02 +0500

    get real data in class and sub component

--- web/src/modules/TIP/Card.tsx
+++ web/src/modules/TIP/Card.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router-dom'
 
 interface P {
     className: string
+    teacher_name: string
+    school_name: string
 }
 
 type PropsType = P & RouteComponentProps
 
-const Card: React.FC<PropsType> = ({ className }) => {
+const Card: React.FC<PropsType> = ({ className, teacher_name, school_name }) => {
 
     return <div className="container sm:px-8 bg-blue-500 rounded m-3 h-20 mb-6">
         <div className="flex flex-row justify-start">
             <img className="h-12 rounded-full p-4" src="https://cdn.dribbble.com/users/2199928/screenshots/11532918/shot-cropped-1590177932366.png?compress=1&resize=400x300" alt="img" />
             <div className="flex flex-col justify-center">
-                <div className="text-white text-base font-medium">Miss Humna</div>
-                <div className="text-white text-xs font-thin">The Educator School</div>
+                <div className="text-white text-base font-medium">{teacher_name}</div>
+                <div className="text-white text-xs font-thin">{school_name}</div>
             </div>
             {className && <div className="text-white text-extrabold flex items-center justify-center m-5 bg-blue-300 rounded-full h-10 w-10">{className}</div>}
         </div>
     </div>
 }
 
-export default Card
+export default connect((state: RootReducerState) => ({
+    teacher_name: state.auth.name,
+    school_name: state.auth.school_id,
+}))(Card)
--- web/src/modules/TIP/Classes.tsx
+++ web/src/modules/TIP/Classes.tsx
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router-dom'
+import { getSectionsFromClasses } from 'utils/getSectionsFromClasses'
 
 interface P {
     setClassName: (className: string) => any
+    classes: RootDBState["classes"]
 }
 
 type PropsType = P & RouteComponentProps
 
-const classes = [{ class: 1, color: "bg-blue-300" }, { class: 2, color: "bg-yellow-400" }, { class: 3, color: "bg-gray-400" }, { class: 4, color: "bg-blue-900" }, { class: 5, color: "bg-pink-500" }]
-const Classes: React.FC<PropsType> = ({ setClassName }) => {
+// const classes = [{ class: 1, color: "bg-blue-300" }, { class: 2, color: "bg-yellow-400" }, { class: 3, color: "bg-gray-400" }, { class: 4, color: "bg-blue-900" }, { class: 5, color: "bg-pink-500" }]
+const Classes: React.FC<PropsType> = ({ setClassName, classes }) => {
 
     const test = (e: any) => {
         setClassName(e.target.id)
     }
 
+    const sortedSections = useMemo(() => getSectionsFromClasses(classes).sort((a, b) => (a.classYear || 0) - (b.classYear || 0)), [])
+
     return <div className="flex flex-wrap flex-row justify-around w-full mx-4">
-        {classes.map((classObj) => (
-            <div key={classObj.class} className="flex-wrap container w-2/5 sm:px-8 bg-white rounded-lg m-3 h-28 flex items-center justify-start flex-col shadow-lg" id={`${classObj.class}`} onClick={test}>
-                <div className={`text-white text-bold flex items-center justify-center m-5 rounded-full h-10 w-10 ${classObj.color}`}>{classObj.class}</div>
-                <div className="text-blue-900">Class</div>
-            </div>
-        ))}
+        {
+            sortedSections.map((classObj) => {
+                return <div key={classObj.id} className="flex-wrap container w-2/5 sm:px-8 bg-white rounded-lg m-3 h-28 flex items-center justify-start flex-col shadow-lg" id={(classObj.namespaced_name).substring(6)} onClick={test}>
+                    <div className={`text-white text-bold flex items-center justify-center m-5 rounded-full h-10 w-10 bg-blue-900`}>{(classObj.namespaced_name).substring(6)}</div>
+                    <div className="text-blue-900">Class</div>
+                </div>
+            })
+        }
     </div >
 }
 
-export default Classes
+export default connect((state: RootReducerState) => ({
+    classes: state.db.classes,
+    teacher_name: state.auth.name,
+    school_name: state.auth.school_id,
+}))(Classes)
