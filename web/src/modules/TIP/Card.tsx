1394cde1334526ebaf99207177cf52850eff80e8
Humna Aleem <humna.aleem@cerp.org.pk>
2021-01-05 14:23:53 +0500

    add icons of home page and set font size

--- web/src/modules/TIP/Card.tsx
+++ web/src/modules/TIP/Card.tsx
@@ -11,10 +11,10 @@ const Card: React.FC<P> = ({ class_name, teacher_name, school_name }) => {
 
     return <div className="container sm:px-8 bg-blue-500 rounded m-3 h-20 mb-6">
         <div className="flex flex-row justify-start">
-            <img className="h-12 rounded-full p-4" src="https://cdn.dribbble.com/users/2199928/screenshots/11532918/shot-cropped-1590177932366.png?compress=1&resize=400x300" alt="img" />
+            <img className="w-12 h-12 rounded-full p-4" src="https://cdn.dribbble.com/users/2199928/screenshots/11532918/shot-cropped-1590177932366.png?compress=1&resize=400x300" alt="img" />
             <div className="flex flex-col justify-center">
-                <div className="text-white text-base font-medium">{teacher_name}</div>
-                <div className="text-white text-xs font-thin">{school_name}</div>
+                <div className="text-white text-lg">{teacher_name}</div>
+                <div className="text-white text-base">{school_name}</div>
             </div>
             {class_name && <div className="text-white text-extrabold flex items-center justify-center m-5 bg-blue-300 rounded-full h-10 w-10">{(class_name).substring(6)}</div>}
         </div>
--- web/src/modules/TIP/Headings.tsx
+++ web/src/modules/TIP/Headings.tsx
@@ -8,8 +8,8 @@ interface P {
 const Headings: React.FC<P> = ({ heading, sub_heading }) => {
 
     return <div className="w-full">
-        <div className="text-blue-900 text-lg font-medium text-center">{heading}</div>
-        <div className="text-blue-900 text-sm font-thin text-center">{sub_heading}</div>
+        <div className="text-blue-900 text-2xl text-center">{heading}</div>
+        <div className="text-blue-900 text-lg text-center">{sub_heading}</div>
     </div>
 }
 
