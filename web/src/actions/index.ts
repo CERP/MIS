b747ca8736bed432db10d5c36ec5d0c10583119a
Humna Aleem <humna.aleem@cerp.org.pk>
2021-01-11 23:47:39 +0500

    modified path according to new object

--- web/src/actions/index.ts
+++ web/src/actions/index.ts
@@ -548,11 +548,11 @@ export const addPayment = (student: MISStudent, payment_id: string, amount: numb
 export const addReport = (student_id: string, diagnostic_report: MISDiagnosticReport['questions'], test_id: string) => (dispatch: Function) => {
 	dispatch(createMerges([
 		{
-			path: ["db", "students", student_id, "diagnostic_result", test_id, "questions"],
+			path: ["db", "students", student_id, "targeted_instruction", "diagnostic_result", test_id, "questions"],
 			value: diagnostic_report
 		},
 		{
-			path: ["db", "students", student_id, "diagnostic_result", test_id, "checked"],
+			path: ["db", "students", student_id, "targeted_instruction", "diagnostic_result", test_id, "checked"],
 			value: true
 		}
 	]))
