5fe676223df5546cb991d96f36e0132eb8660c33
mudassar045 <mudassaralics007@gmail.com>
2021-01-11 14:15:35 +0500

    fix css for client cards

--- web/src/pages/landing/index.tsx
+++ web/src/pages/landing/index.tsx
@@ -100,7 +100,7 @@ export const Landing = () => {
 							<div className="text-gray-600">Students</div>
 						</div>
 						<div className="flex flex-col items-center space-y-2">
-							<div className="h-24 w-24 rounded-full bg-orange-brand mb-4 shadow-md">
+							<div className="h-24 w-24 rounded-full m-8 bg-orange-brand shadow-md">
 								<img className="w-16 h-16 m-4" src={iconParent} alt="parent" />
 							</div>
 							<div className="text-2xl font-bold">50,000+</div>
@@ -110,31 +110,31 @@ export const Landing = () => {
 
 				</div>
 
-				<div className="mt-20">
-					<div className=" text-3xl font-semibold text-center">Our Clients</div>
-					<div className="flex flex-row flex-wrap items-center  justify-center space-y-4 md:space-x-16 md:space-y-0 mt-10">
-						<div className="h-36 w-36 rounded-full bg-orange-brand mb-4">
-							<img className="w-20 h-20 m-8 shadow-md rounded-full" src="favicon.ico" alt="city" />
+				<div className="mt-20 px-20">
+					<div className="text-3xl font-semibold text-center">Our Clients</div>
+					<div className="grid grid-cols-1 md:grid-cols-5 gap-6 mt-10">
+						<div className="h-36 w-36 rounded-full bg-orange-brand mb-4 mx-auto">
+							<img className="w-20 h-20 m-8 shadow-md rounded-full" src="favicon.ico" alt="a1" />
 						</div>
 
-						<div className="h-36 w-36 rounded-full bg-teal-500 mb-4">
-							<img className="w-20 h-20 m-8 shadow-md rounded-full" src="favicon.ico" alt="city" />
+						<div className="h-36 w-36 rounded-full bg-teal-500 mb-4 mx-auto">
+							<img className="w-20 h-20 m-8 shadow-md rounded-full" src="favicon.ico" alt="a2" />
 						</div>
-						<div className="h-36 w-36 rounded-full bg-red-brand mb-4">
-							<img className="w-20 h-20 m-8 shadow-md rounded-full" src="favicon.ico" alt="city" />
+						<div className="h-36 w-36 rounded-full bg-red-brand mb-4 mx-auto">
+							<img className="w-20 h-20 m-8 shadow-md rounded-full" src="favicon.ico" alt="a3" />
 						</div>
-						<div className="h-36 w-36 rounded-full bg-teal-500 mb-4">
-							<img className="w-20 h-20 m-8 shadow-md rounded-full" src="favicon.ico" alt="city" />
+						<div className="h-36 w-36 rounded-full bg-teal-500 mb-4 mx-auto">
+							<img className="w-20 h-20 m-8 shadow-md rounded-full" src="favicon.ico" alt="a4" />
 						</div>
-						<div className="h-36 w-36 rounded-full bg-orange-brand mb-4">
-							<img className="w-20 h-20 m-8 shadow-md rounded-full" src="favicon.ico" alt="city" />
+						<div className="h-36 w-36 rounded-full bg-orange-brand mb-4 mx-auto">
+							<img className="w-20 h-20 m-8 shadow-md rounded-full" src="favicon.ico" alt="a5" />
 						</div>
 					</div>
 				</div>
 
-				<div className="mt-20">
+				<div className="mt-20 px-20">
 					<div className=" text-3xl font-semibold text-center">Features</div>
-					<div className="grid mt-10">
+					<div className="mt-10">
 						<div className="grid md:gap-12 grid-cols-1 md:grid-cols-5 mx-auto space-y-4 md:space-y-0">
 							<div className="px-6 py-14 bg-yellow-300 rounded-xl">
 								<img className="w-24 h-24 mx-auto rounded-full shadow-md" src={iconPlug} alt="on-off" />
@@ -154,7 +154,6 @@ export const Landing = () => {
 							<div className="px-6 py-14 bg-blue-300 rounded-xl">
 								<img className="w-24 h-24 mx-auto rounded-full shadow-md" src={iconCall} alt="call" />
 								<div className="text-white text-lg text-center mt-2">Excellent Service</div>
-
 							</div>
 
 							<div className="px-6 py-14 bg-yellow-300 rounded-xl">
