f158c33007faa88bef6322e347da140132392669
Humna Aleem <humna.aleem@cerp.org.pk>
2021-01-14 16:14:22 +0500

    create single std result

--- web/src/modules/TIP/FormativeTest/ResultCards/Result.tsx
+++ web/src/modules/TIP/FormativeTest/ResultCards/Result.tsx
@@ -1,29 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom'
 import Footer from '../../Footer'
 import Headings from '../../Headings'
-import SingleStdResultCard from './SingleStdResultCard'
-import ClassResultCard from './ClassResultCard'
+import ChildView from './ChildView'
+import SkillView from './SkillView'
+import SingleStdView from './SingleStdView'
 
 interface P {
 }
 
 type PropsType = P & RouteComponentProps
 
-const Result: React.FC<PropsType> = (props) => {
+const Result: React.FC<PropsType> = () => {
+
+    const [type, setType] = useState('skill_view')
 
     return <div className="flex flex-wrap content-between">
         <Headings heading="Formative Test Result" sub_heading="" />
-        <div className="flex flex-row justify-around w-full my-3 mx-6">
-            <button className="border-none rounded-3xl text-white bg-blue-900 py-1 px-6">Skill View</button>
-            <button className="rounded-3xl text-blue-900 broder border-solid border-blue-900 py-1 px-6 bg-white">Child View</button>
+        {type === 'single_std_view' ? <div className="flex flex-row justify-center w-full">
+            <div className="bg-blue-900 h-5 my-3 w-3/4 rounded-3xl py-1 px-3">
+                <img className="h-7 w-8 rounded-full pl-0 absolute left-7 top-11" src="https://cdn.dribbble.com/users/2199928/screenshots/11532918/shot-cropped-1590177932366.png?compress=1&resize=400x300" alt="img" />
+                <div className="text-white flex justify-center">Child View - Humna</div>
+            </div>
+        </div> :
+            <div className="flex flex-row justify-around w-full my-3 mx-6">
+                <button className={type === 'skill_view' ? "border-none rounded-3xl text-white bg-blue-900 py-1 px-6 outline-none" :
+                    "rounded-3xl text-blue-900 broder border-solid border-blue-900 py-1 px-6 bg-white outline-none"}
+                    onClick={() => setType('skill_view')}>Skill View</button>
+                <button className={type === 'child_view' ? "border-none rounded-3xl text-white bg-blue-900 py-1 px-6 outline-none" :
+                    "rounded-3xl text-blue-900 broder border-solid border-blue-900 py-1 px-6 bg-white outline-none"}
+                    onClick={() => setType('child_view')}>Child View</button>
+            </div>
+        }
+        <div className={`flex flex-row ${type === 'child_view' ? "justify-around" : "justify-between px-8"} h-7 items-center text-white text-xs bg-blue-900 w-full mb-21`}>
+            {type === 'skill_view' && <>
+                <div>skill</div>
+                <div>Class Average</div>
+            </>}
+            {type === 'child_view' && <>
+                <div>Name</div>
+                <div className="flex flex-row justify-between w-3/12">
+                    <div>Score</div>
+                    <div>%</div>
+                </div>
+            </>}
+            {type === 'single_std_view' && <>
+                <div>skill</div>
+                <div className="flex flex-row justify-between w-3/12">
+                    <div>Score</div>
+                    <div>%</div>
+                </div>
+            </>}
         </div>
-        <div className="flex flex-row justify-around h-7 items-center text-white px-4 text-xs bg-blue-900 w-full mb-2">
-            <div>skill</div>
-            <div>Class Average</div>
-        </div>
-        <SingleStdResultCard />
-        <ClassResultCard />
+        {type === 'skill_view' && <>
+            <SkillView slo="2 digits Multiplication" percentage="70%" />
+            <SkillView slo="2 digits Addition" percentage="30%" />
+            <SkillView slo="2 digits Division" percentage="50%" />
+        </>}
+        {type === 'child_view' && <>
+            <ChildView name="Humna" score={4} percentage="70%" setType={setType} />
+            <ChildView name="Rehan" score={4} percentage="50%" setType={setType} />
+            <ChildView name="Ali" score={4} percentage="60%" setType={setType} />
+        </>}
+        {type === 'single_std_view' && <>
+            <SingleStdView slo="2 digits Multiplication" score={4} percentage="70%" />
+            <SingleStdView slo="2 digits Addition" score={4} percentage="50%" />
+            <SingleStdView slo="2 digits Division" score={4} percentage="60%" />
+        </>}
         <Footer />
     </div>
 }
--- web/src/modules/TIP/FormativeTest/ResultCards/SkillView.tsx
+++ web/src/modules/TIP/FormativeTest/ResultCards/SkillView.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { ArrowBack } from 'assets/icons'
 
 interface P {
+    slo: string
+    percentage: string
 }
 
-const ClassResultCard: React.FC<P> = (props) => {
+const SkillView: React.FC<P> = ({ slo, percentage }) => {
 
     return <div className="bg-blue-200 text-blue-900 rounded-md flex flex-row justify-between items-center px-3 my-2 h-11 shadow-lg w-full">
-        <div className="flex flex-row justify-between items-center w-full">
-            <div className="w-full flex flex-row justify-around items-center">
-                <div>2 digits Addition</div>
-                <div>70%</div>
+        <div className="flex flex-row justify-between w-full">
+            <div className="w-full flex flex-row justify-between px-3 items-center text-left">
+                <div className="font-bold">{slo}</div>
+                <div>{percentage}</div>
             </div>
             <div className="bg-white rounded-full h-7 w-7 flex justify-center items-center">
                 <img className="h-3" src={ArrowBack} />
@@ -19,4 +21,4 @@ const ClassResultCard: React.FC<P> = (props) => {
     </div>
 }
 
-export default ClassResultCard
+export default SkillView
