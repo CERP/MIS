7f72724287d68ca37934f72dcc60dd0b495cb869
Humna Aleem <humna.aleem@cerp.org.pk>
2021-01-20 10:35:42 +0500

    done lesson plan functionality

--- web/src/modules/TIP/index.tsx
+++ web/src/modules/TIP/index.tsx
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { connect } from 'react-redux';
 import { RouteComponentProps, Link } from 'react-router-dom'
 import { Formative, LessonPlans, Diagnostic, Videos } from 'assets/icons'
+import { getCount } from 'utils/TIP'
 import Headings from './Headings'
 import Card from './Card'
 interface P {
+    faculty: RootDBState["faculty"]
+    faculty_id: string
 }
 
 type PropsType = P & RouteComponentProps
 
-const Home: React.FC<PropsType> = (props) => {
+const Home: React.FC<PropsType> = ({ faculty, faculty_id }) => {
+
+    const count = useMemo(() => getCount(faculty, faculty_id), [])
 
     return <div className="flex flex-wrap content-between bg-white">
-        <Card {...props} class_name='' />
+        <Card class_name='' />
         <Headings heading="Welcome to TIP" sub_heading="What would you like to do today ?" />
-        {/* <Link className="container sm:px-8 bg-white rounded-2xl m-3 h-44 flex flex-col content-center items-center shadow-lg no-underline"
+        {count > 2 ? <><Link className="container sm:px-8 bg-white rounded-2xl m-3 h-44 flex flex-col content-center items-center shadow-lg no-underline"
             to={'/targeted-instruction/formative-test'}>
             <img className="h-24 py-4 pr-4 w-28" src={Formative} alt="img" />
             <div className="text-blue-900 text-lg font-bold">Formative Test</div>
-        </Link> */}
-        <Link className="container sm:px-8 bg-white rounded-2xl m-3 h-44 flex flex-col content-center items-center shadow-lg no-underline"
-            to={'/targeted-instruction/formative-test'}>
-            <img className="h-24 py-4 w-14" src={Diagnostic} alt="img" />
-            <div className="text-blue-900 text-lg font-bold">Diagnostic Test</div>
         </Link>
-        {/* <Link className="container sm:px-8 bg-white rounded-xl m-3 h-28 flex flex-row justify-around w-full items-center shadow-lg no-underline"
-            to={"/targeted-instruction/diagnostic-result"}>
-            <img className="h-12 py-4 pl-4" src={Diagnostic} alt="img" />
-            <div className="text-blue-900 text-lg font-bold">Diagnostic Test Results</div>
-        </Link> */}
+            <Link className="container sm:px-8 bg-white rounded-xl m-3 h-28 flex flex-row justify-around w-full items-center shadow-lg no-underline"
+                to={"/targeted-instruction/diagnostic-result"}>
+                <img className="h-12 py-4 pl-4" src={Diagnostic} alt="img" />
+                <div className="text-blue-900 text-lg font-bold">Diagnostic Test Results</div>
+            </Link></> :
+            <Link className="container sm:px-8 bg-white rounded-2xl m-3 h-44 flex flex-col content-center items-center shadow-lg no-underline"
+                to={'/targeted-instruction/formative-test'}>
+                <img className="h-24 py-4 w-14" src={Diagnostic} alt="img" />
+                <div className="text-blue-900 text-lg font-bold">Diagnostic Test</div>
+            </Link>}
         <div className="flex flex-row content-center items-center justify-center w-full">
             <Link className="container sm:px-8 bg-white rounded-xl m-3 h-40 flex flex-col content-center items-center shadow-lg no-underline"
                 to={"/targeted-instruction/training-videos"}>
@@ -43,4 +49,7 @@ const Home: React.FC<PropsType> = (props) => {
     </div>
 }
 
-export default Home
+export default connect((state: RootReducerState) => ({
+    faculty: state.db.faculty,
+    faculty_id: state.auth.faculty_id
+}))(Home)
