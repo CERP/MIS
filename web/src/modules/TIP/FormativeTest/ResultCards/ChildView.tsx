fe8c9dbf451e5b205723aada3d507502f0a9cf5a
Humna Aleem <humna.aleem@cerp.org.pk>
2021-01-22 16:23:37 +0500

    change icons

--- web/src/modules/TIP/FormativeTest/ResultCards/ChildView.tsx
+++ web/src/modules/TIP/FormativeTest/ResultCards/ChildView.tsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import { ArrowBack } from 'assets/icons'
-import SingleStdView from './SingleStdView'
 
 interface P {
     name: string
     obtain: number
     total: number
-    slo_obj: {}
+    id: string
+
+    setType: (type: string) => void
 }
 
-const ChildView: React.FC<P> = ({ name, obtain, total, slo_obj }) => {
-    const [type, setType] = useState(false)
+const ChildView: React.FC<P> = ({ name, obtain, total, setType, id }) => {
+    const [idd, setId] = useState('')
+
+    const redirect = () => {
+        setType('single_std_view')
+        setId(id)
+    }
     //@ts-ignore
     const percentage = parseInt((obtain / total) * 100)
-    return <>{!type && <div className={`${percentage >= 60 ? "bg-green-primary text-white" :
+    return <div className={`${percentage >= 60 ? "bg-green-primary text-white" :
         percentage >= 50 ? "bg-orange-primary text-white" : "bg-red-primary text-white"} 
-    flex flex-row justify-between items-center px-3 my-2 h-11 shadow-lg w-full`} onClick={() => setType(true)}>
+    flex flex-row justify-between items-center px-3 my-2 h-11 shadow-lg w-full`} onClick={redirect}>
         <div className="flex flex-row justify-between items-center w-full">
             <div className="w-3/5 flex flex-row justify-start content-center items-center">
                 <img className="h-6 rounded-full p-3" src="https://cdn.dribbble.com/users/2199928/screenshots/11532918/shot-cropped-1590177932366.png?compress=1&resize=400x300" alt="img" />
@@ -29,13 +35,7 @@ const ChildView: React.FC<P> = ({ name, obtain, total, slo_obj }) => {
                 <img className="h-3" src={ArrowBack} />
             </div>
         </div>
-    </div>}
-        <>{type && Object.entries(slo_obj || {}).map(([slo, obj]) => {
-            //@ts-ignore
-            return <SingleStdView key={slo} slo={slo} obtain={obj.obtain} total={obj.total} />
-        })
-        }</>
-    </>
+    </div>
 }
 
 export default ChildView
--- web/src/modules/TIP/index.tsx
+++ web/src/modules/TIP/index.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react';
 import { connect } from 'react-redux';
 import { RouteComponentProps, Link } from 'react-router-dom'
-import { Formative, LessonPlans, Diagnostic, Videos } from 'assets/icons'
+import { Formative, LessonPlans, Diagnostic, Videos, DiagnosticItalic, Summative } from 'assets/icons'
 import { getCount } from 'utils/TIP'
 import Headings from './Headings'
 import Card from './Card'
@@ -22,7 +22,7 @@ const Home: React.FC<PropsType> = ({ faculty, faculty_id }) => {
         {complete / 2 === count ?
             <Link className="container sm:px-8 bg-white rounded-2xl m-3 h-44 flex flex-col content-center items-center shadow-lg no-underline"
                 to={'/targeted-instruction/summative-test'}>
-                <img className="h-24 py-4 w-14" src={Diagnostic} alt="img" />
+                <img className="h-24 py-4 pr-4 w-28" src={Summative} alt="img" />
                 <div className="text-blue-900 text-lg font-bold">Summative Test</div>
             </Link> :
             count > 2 ? <Link className="container sm:px-8 bg-white rounded-2xl m-3 h-44 flex flex-col content-center items-center shadow-lg no-underline"
@@ -32,7 +32,7 @@ const Home: React.FC<PropsType> = ({ faculty, faculty_id }) => {
             </Link> :
                 <Link className="container sm:px-8 bg-white rounded-2xl m-3 h-44 flex flex-col content-center items-center shadow-lg no-underline"
                     to={'/targeted-instruction/diagnostic-test'}>
-                    <img className="h-24 py-4 w-14" src={Diagnostic} alt="img" />
+                    <img className="h-24 py-4 w-14" src={DiagnosticItalic} alt="img" />
                     <div className="text-blue-900 text-lg font-bold">Diagnostic Test</div>
                 </Link>}
         <div className="flex flex-row content-center items-center justify-center w-full">
@@ -49,13 +49,13 @@ const Home: React.FC<PropsType> = ({ faculty, faculty_id }) => {
         </div>
         {count > 2 && complete / 2 !== count && <Link className="container sm:px-8 bg-white rounded-xl m-3 h-28 flex flex-row justify-around w-full items-center shadow-lg no-underline"
             to={"/targeted-instruction/diagnostic-result"}>
-            <img className="h-12 py-4 pl-4" src={Diagnostic} alt="img" />
+            <img className="h-12 py-4 pl-4" src={DiagnosticItalic} alt="img" />
             <div className="text-blue-900 text-lg font-bold">Diagnostic Test Results</div>
         </Link>}
         {complete / 2 === count && <div className="flex flex-row content-center items-center justify-center w-full">
             <Link className="container sm:px-8 bg-white rounded-xl m-3 h-40 flex flex-col content-center items-center shadow-lg no-underline"
                 to={"/targeted-instruction/diagnostic-result"}>
-                <img className="h-12 py-6" src={Diagnostic} alt="img" />
+                <img className="h-12 py-6" src={DiagnosticItalic} alt="img" />
                 <div className="text-base text-blue-900 font-bold text-center">Diagnostic Test Results</div>
             </Link>
             <Link className="container sm:px-8 bg-white rounded-xl m-3 h-40 flex flex-col content-center items-center shadow-lg no-underline"
