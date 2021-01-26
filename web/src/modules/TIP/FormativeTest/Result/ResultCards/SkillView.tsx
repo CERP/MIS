09108cc3905181714b8eaf4b4ea22b48638fc6bd
Humna Aleem <humna.aleem@cerp.org.pk>
2021-01-26 14:35:41 +0500

    display slo based result

--- web/src/modules/TIP/FormativeTest/Result/ResultCards/SkillView.tsx
+++ web/src/modules/TIP/FormativeTest/Result/ResultCards/SkillView.tsx
@@ -5,17 +5,26 @@ interface P {
     slo: string
     obtain: number
     total: number
+
+    setType: (type: string) => void
+    setSlo: (slo: string) => void
 }
 
-const SkillView: React.FC<P> = ({ slo, obtain, total }) => {
+const SkillView: React.FC<P> = ({ slo, obtain, total, setType, setSlo }) => {
 
     const percentage = Math.trunc((obtain / total) * 100)
 
+    const setValues = (slo: string) => {
+        setType('single_slo_view')
+        setSlo(slo.replace(',', '$'))
+    }
+
     return <div className={`${percentage >= 60 ? "bg-green-250" :
-        percentage >= 50 ? "bg-yellow-250" : "bg-red-250"} flex flex-row justify-between items-center px-3 my-1 h-14 shadow-lg w-full`}>
+        percentage >= 50 ? "bg-yellow-250" : "bg-red-250"} flex flex-row justify-between items-center px-3 my-1 h-14 shadow-lg w-full`}
+        onClick={() => setValues(slo)} >
         < div className="flex flex-row justify-between w-full" >
             <div className="w-full flex flex-row justify-between px-3 items-center text-left">
-                <div className="font-bold">{slo}</div>
+                <div className="font-bold">{slo.replace('$', ',')}</div>
                 <div className="text-xs">{`${percentage}%`}</div>
             </div>
             <div className="bg-white rounded-full h-7 w-9 flex justify-center items-center">
