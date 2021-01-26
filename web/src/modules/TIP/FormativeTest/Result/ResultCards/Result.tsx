30d0356e1246e1e1fffc0027d6724ac0525c9015
Humna Aleem <humna.aleem@cerp.org.pk>
2021-01-26 11:32:31 +0500

    improve css of summative test module

--- web/src/modules/TIP/FormativeTest/Result/ResultCards/Result.tsx
+++ web/src/modules/TIP/FormativeTest/Result/ResultCards/Result.tsx
@@ -29,15 +29,20 @@ const Result: React.FC<PropsType> = (props) => {
 
     return <div className="flex flex-wrap content-between mt-3">
         <Headings heading={url[2] === "formative-test" ? "Formative Test Result" : "Summative Test Result"} sub_heading="" />
-        {type === 'single_std_view' ? <div className="flex flex-row justify-center w-full" onClick={() => setType('child_view')}>
-            <div className="bg-blue-primary h-5 my-3 w-3/4 rounded-3xl py-1 px-3">
-                <img className="h-7 w-8 rounded-full pl-0 absolute left-7 top-14" src="https://cdn.dribbble.com/users/2199928/screenshots/11532918/shot-cropped-1590177932366.png?compress=1&resize=400x300" alt="img" />
-                <div className="text-white flex justify-center">Child View - {name}</div>
+        {type === 'single_std_view' ? url[2] === "summative-test" ? <div className="flex flex-row justify-center w-full" onClick={() => setType('child_view')}>
+            <div className={`bg-${group}-primary h-6 my-3 w-3/4 rounded-3xl py-1 px-3 flex justify-center items-center`}>
+                <img className="h-8 w-8 rounded-full pl-0 absolute left-7 top-14" src="https://cdn.dribbble.com/users/2199928/screenshots/11532918/shot-cropped-1590177932366.png?compress=1&resize=400x300" alt="img" />
+                <div className="text-white flex justify-center capitalize">{`${name} | ${group} Group`}</div>
             </div>
-        </div> :
+        </div> : <div className="flex flex-row justify-center w-full" onClick={() => setType('child_view')}>
+                <div className="bg-blue-primary h-5 my-3 w-3/4 rounded-3xl py-1 px-3">
+                    <img className="h-7 w-8 rounded-full pl-0 absolute left-7 top-14" src="https://cdn.dribbble.com/users/2199928/screenshots/11532918/shot-cropped-1590177932366.png?compress=1&resize=400x300" alt="img" />
+                    <div className="text-white flex justify-center">Child View - {name}</div>
+                </div>
+            </div> :
             url[2] === "summative-test" ?
                 <div className="flex flex-row justify-around w-full my-3 mx-6">
-                    <button className={`rounded-md text-white border-none bg-${group}-primary py-2 outline-none w-4/6 text-lg font-bold `}
+                    <button className={`rounded-md text-white border-none bg-${group}-primary py-2 outline-none w-5/6 text-lg font-bold `}
                     >Group {class_name}</button>
                 </div> :
                 <div className="flex flex-row justify-around w-full my-3 mx-6">
@@ -49,7 +54,7 @@ const Result: React.FC<PropsType> = (props) => {
                         onClick={() => setType('child_view')}>Child View</button>
                 </div>
         }
-        <div className={`flex flex-row ${type === 'child_view' ? "justify-around" : "justify-between px-8"} h-7 items-center text-white text-xs bg-blue-primary w-full mb-21`}>
+        <div className={`flex flex-row ${type === 'child_view' ? "justify-around" : "justify-between px-8"} h-7 items-center text-white text-xs bg-blue-primary w-full mb-1`}>
             {type === 'skill_view' && <>
                 <div className="font-bold">skill</div>
                 <div className="font-bold">Class Average</div>
--- web/src/modules/TIP/FormativeTest/Result/ResultCards/SingleStdView.tsx
+++ web/src/modules/TIP/FormativeTest/Result/ResultCards/SingleStdView.tsx
@@ -12,10 +12,10 @@ const SingleStdView: React.FC<P> = ({ slo, obtain, total }) => {
 
     return <div className={`${percentage >= 60 ? "bg-green-250" :
         percentage >= 50 ? "bg-yellow-250" : "bg-red-250"}
-     flex flex-row justify-between items-center px-3 my-2 h-11 shadow-lg w-full`}>
+     flex flex-row justify-between items-center px-3 my-1 h-14 shadow-lg w-full`}>
         <div className="flex flex-row justify-between w-full">
             <div className="w-full flex flex-row justify-between px-3 items-center text-left">
-                <div className="">{slo.replace("$", ",")}</div>
+                <div className="font-bold">{slo.replace("$", ",")}</div>
                 <div className="flex flex-row justify-between w-3/12 text-xs">
                     <div>{`${obtain}/${total}`}</div>
                     <div>{`${percentage}%`}</div>
