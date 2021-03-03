06b6220bc2321fd40f83ed4ff82ba570021eaa2e
Mudassar Ali <Mudassaralics007@gmail.com>
2021-03-03 17:37:19 +0500

    add reset fee confirmation modal

--- web/src/pages/school/fees/reset/index.tsx
+++ web/src/pages/school/fees/reset/index.tsx
@@ -6,6 +6,9 @@ import { isValidStudent } from 'utils'
 import { toTitleCase } from 'utils/toTitleCase'
 import getSectionsFromClasses from 'utils/getSectionsFromClasses'
 import { AppLayout } from 'components/Layout/appLayout'
+import { resetFees } from 'actions'
+import { useComponentVisible } from 'utils/customHooks'
+import { TModal } from 'components/Modal'
 
 enum ResetFeeOptions {
 	EMPTY_OPTION,
@@ -27,6 +30,12 @@ export const ResetFee = () => {
 	const dispatch = useDispatch()
 	const { students, classes } = useSelector((state: RootReducerState) => state.db)
 
+	const {
+		ref: confirmResetModalRef,
+		isComponentVisible: showConfirmResetModal,
+		setIsComponentVisible: setShowConfirmResetModal
+	} = useComponentVisible(false)
+
 	const [state, setState] = useState<State>({
 		resetFor: ResetFeeOptions.EMPTY_OPTION,
 		sectionId: '',
@@ -39,7 +48,7 @@ export const ResetFee = () => {
 			.sort((a, b) => (a.classYear ?? 0) - (b.classYear ?? 0))
 	}, [classes])
 
-	const resetFee = (): void => {
+	const handleResetFee = (): void => {
 
 		const filteredStudents = Object.values(students).filter(student => {
 
@@ -62,15 +71,20 @@ export const ResetFee = () => {
 			return false
 
 		})
-	}
 
-	console.log(state)
+		// TODO: use RHT
+
+		dispatch(resetFees(filteredStudents))
+
+		// close the modal
+		setShowConfirmResetModal(false)
+	}
 
 	return (
 		<AppLayout title={"Reset Fee"}>
 			<div className="p-5 md:p-10 md:pb-0 relative">
 				<div className="text-2xl font-bold mt-4 mb-8 text-center">Reset Fee</div>
-				<div className="md:w-4/5 md:mx-auto flex flex-col items-center space-y-3 rounded-2xl bg-gray-700 pb-6 my-4 md:mt-8">
+				<div className="min-h-screen md:w-4/5 md:mx-auto flex flex-col items-center space-y-3 rounded-2xl bg-gray-700 pb-6 my-4 md:mt-8">
 					<div className="text-white text-center text-base my-5">Please be careful while reseting Fee</div>
 					<div className="text-white space-y-4 px-4 w-full md:w-3/5">
 						<div>Reset Fee For:</div>
@@ -82,6 +96,19 @@ export const ResetFee = () => {
 							<option value={2}>Single Class</option>
 							<option value={3}>Single Student</option>
 						</select>
+						{showConfirmResetModal &&
+							<TModal>
+								<div className="bg-white md:p-10 p-8 space-y-2 text-center" ref={confirmResetModalRef}>
+									<h1>You are resetting the Fee for</h1>
+									<div className="font-semibold text-lg md:text-xl">{confirmResetForLabels[state.resetFor]}</div>
+									<div>Are you sure you want to reset?</div>
+									<div className="flex flex-row justify-between">
+										<button onClick={() => setShowConfirmResetModal(false)} className="py-1 md:py-2 tw-btn bg-gray-400 text-white">Cancel</button>
+										<button onClick={handleResetFee} className="py-1 md:py-2 tw-btn-red">Confirm</button>
+									</div>
+								</div>
+							</TModal>
+						}
 						{(state.resetFor === ResetFeeOptions.SINGLE_CLASS || state.resetFor === ResetFeeOptions.SINGLE_STUDENT)
 							&& (<>
 								<div>Class</div>
@@ -107,6 +134,7 @@ export const ResetFee = () => {
 									{
 										Object.values(students)
 											.filter(s => isValidStudent(s) && s.section_id === state.sectionId)
+											.sort((a, b) => a.Name.localeCompare(b.Name))
 											.map(s => <option key={s.id} value={s.id}>{toTitleCase(s.Name)}</option>)
 									}
 								</select>
@@ -114,6 +142,7 @@ export const ResetFee = () => {
 						}
 
 						<button
+							onClick={() => setShowConfirmResetModal(!showConfirmResetModal)}
 							disabled={ResetFeeOptions.EMPTY_OPTION === state.resetFor}
 							className={clsx("tw-btn-red w-full", {
 								"opacity-75 pointer-events-none": (ResetFeeOptions.EMPTY_OPTION === state.resetFor
