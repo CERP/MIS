4de7da78da3fa6b96e36fe42e853d891cfae9476
Mudassar Ali <Mudassaralics007@gmail.com>
2021-03-15 13:12:41 +0500

    create new component files

--- web/src/pages/school/fees/index.tsx
+++ web/src/pages/school/fees/index.tsx
@@ -4,7 +4,9 @@ import { RouteComponentProps } from 'react-router-dom'
 
 import { FeeSettings } from './settings'
 import { VoucherSettings } from './voucher-settings'
-import { FeeCollection } from './collection'
+import { FamilyPayments } from './payments/family'
+import { StudentPayments } from './payments/student'
+
 import { ResetFee } from './reset'
 import { PrintVoucher } from './print-voucher'
 
@@ -13,75 +15,78 @@ import { AppLayout } from 'components/Layout/appLayout'
 
 const MenuItems = [
 	{
-		title: "Fee Settings",
-		link: "/school/fees/settings",
-		icon: ""
+		title: 'Fee Settings',
+		link: '/school/fees/settings',
+		icon: ''
 	},
 	{
-		title: "Voucher Settings",
-		link: "/school/fees/voucher-settings",
-		icon: ""
+		title: 'Voucher Settings',
+		link: '/school/fees/voucher-settings',
+		icon: ''
 	},
 	{
-		title: "Print Voucher",
-		link: "/school/fees/print-voucher",
-		icon: ""
+		title: 'Print Voucher',
+		link: '/school/fees/print-voucher',
+		icon: ''
 	},
 	{
-		title: "Fees Collection",
-		link: "/school/fees/collection",
-		icon: ""
+		title: 'Family Payment',
+		link: '/school/fees/family',
+		icon: ''
 	},
 	{
-		title: "Reset Fees",
-		link: "/school/fees/reset",
-		icon: ""
+		title: 'Student Payment',
+		link: '/school/fees/student',
+		icon: ''
+	},
+	{
+		title: 'Reset Fees',
+		link: '/school/fees/reset',
+		icon: ''
 	}
 ]
 
 enum Pages {
-	FEE_SETTINGS = "settings",
-	VOUCHER_SETTINGS = "voucher-settings",
-	PRINT_VOUCHER = "print-voucher",
-	FEE_COLLECTION = "collection",
-	FEE_RESET = "reset"
+	FEE_SETTINGS = 'settings',
+	VOUCHER_SETTINGS = 'voucher-settings',
+	PRINT_VOUCHER = 'print-voucher',
+	FAMILY_PAYMENTS = 'family',
+	STUDENT_PAYMENTS = 'student',
+	FEE_RESET = 'reset'
 }
 
 type Props = RouteComponentProps<{ page: string }>
 
 export const SchoolFees: React.FC<Props> = ({ match }) => {
-
 	const { page } = match.params
 
-	const renderComponent = () => (
+	const renderComponent = () =>
 		cond([
 			[page === Pages.FEE_SETTINGS, <FeeSettings key={Pages.FEE_SETTINGS} />],
 			[page === Pages.VOUCHER_SETTINGS, <VoucherSettings key={Pages.VOUCHER_SETTINGS} />],
 			[page === Pages.PRINT_VOUCHER, <PrintVoucher key={Pages.PRINT_VOUCHER} />],
-			[page === Pages.FEE_COLLECTION, <FeeCollection key={Pages.FEE_COLLECTION} />],
-			[page === Pages.FEE_RESET, <ResetFee key={Pages.FEE_RESET} />],
+			[page === Pages.FAMILY_PAYMENTS, <FamilyPayments key={Pages.FAMILY_PAYMENTS} />],
+			[page === Pages.STUDENT_PAYMENTS, <StudentPayments key={Pages.STUDENT_PAYMENTS} />],
+
+			[page === Pages.FEE_RESET, <ResetFee key={Pages.FEE_RESET} />]
 		])
-	)
 
 	return (
 		<>
-			{
-				page === undefined ?
-					<AppLayout title={"School Fees"}>
-						<div className="p-6 md:w-2/5 mx-auto">
-							<div className="text-center text-2xl mb-4 font-bold">School Fee</div>
-							<div className="space-y-4">
-								{
-									MenuItems.map((item, index) => (
-										<Card key={item.link + index} {...item} />
-									))
-								}
-							</div>
+			{page === undefined ? (
+				<AppLayout title={'School Fees'}>
+					<div className="p-6 md:w-2/5 mx-auto">
+						<div className="text-center text-2xl mb-4 font-bold">School Fee</div>
+						<div className="space-y-4">
+							{MenuItems.map((item, index) => (
+								<Card key={item.link + index} {...item} />
+							))}
 						</div>
-					</AppLayout>
-					:
-					renderComponent()
-			}
+					</div>
+				</AppLayout>
+			) : (
+				renderComponent()
+			)}
 		</>
 	)
 }
---
+++ web/src/pages/school/fees/payments/family.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { AppLayout } from 'components/Layout/appLayout'
+
+export const FamilyPayments = () => {
+	return (
+		<AppLayout title={'Family Payments'}>
+			<div className="p-5 md:p-10 md:pb-0 relative">
+				<div className="text-2xl font-bold mt-4 mb-8 text-center">Family Payments</div>
+			</div>
+		</AppLayout>
+	)
+}
