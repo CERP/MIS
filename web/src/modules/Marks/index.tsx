656752942307bf1947d162786dd127458d266dfc
Mudassar Ali <Mudassaralics007@gmail.com>
2021-03-22 15:18:41 +0500

    fix: conflict with  custom and tailwindcss

--- web/src/modules/Marks/index.tsx
+++ web/src/modules/Marks/index.tsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 import { getSectionsFromClasses } from 'utils/getSectionsFromClasses'
 import Layout from 'components/Layout'
 import { RouteComponentProps } from 'react-router'
-import Former from 'utils/former';
-import moment from 'moment';
+import Former from 'utils/former'
+import moment from 'moment'
 import { EditIcon, DeleteIcon } from 'assets/icons'
 import getStudentExamMarksSheet from 'utils/studentExamMarksSheet'
 import chunkify from 'utils/chunkify'
@@ -18,14 +18,13 @@ import { ExamTitles } from 'constants/exam'
 import './style.css'
 
 type propsType = {
-	classes: RootDBState["classes"]
-	students: RootDBState["students"]
-	exams: RootDBState["exams"]
-	grades: RootDBState["settings"]["exams"]["grades"]
+	classes: RootDBState['classes']
+	students: RootDBState['students']
+	exams: RootDBState['exams']
+	grades: RootDBState['settings']['exams']['grades']
 	schoolName: string
 
 	deleteExam: (students: Array<string>, exam_id: string) => void
-
 } & RouteComponentProps
 
 type S = {
@@ -41,7 +40,6 @@ type S = {
 }
 
 class Reports extends Component<propsType, S> {
-
 	former: Former
 	constructor(props: propsType) {
 		super(props)
@@ -50,7 +48,7 @@ class Reports extends Component<propsType, S> {
 
 		const section_id = parsed_query.section_id ? parsed_query.section_id.toString() : ''
 		const exam_title = parsed_query.exam_title ? parsed_query.exam_title.toString() : ''
-		const year = parsed_query.year ? parsed_query.year.toString() : moment().format("YYYY")
+		const year = parsed_query.year ? parsed_query.year.toString() : moment().format('YYYY')
 		const month = parsed_query.month ? parsed_query.month.toString() : ''
 
 		this.state = {
@@ -69,7 +67,6 @@ class Reports extends Component<propsType, S> {
 	}
 
 	deleteExam = (exam_id: string): void => {
-
 		if (!window.confirm('Are you sure you want to delete?')) {
 			return
 		}
@@ -82,14 +79,13 @@ class Reports extends Component<propsType, S> {
 			banner: {
 				active: true,
 				good: false,
-				text: "Exam Deleted"
+				text: 'Exam Deleted'
 			}
 		})
 
 		this.props.deleteExam(students, exam_id)
 
 		setTimeout(() => this.setState({ banner: { active: false } }), 3000)
-
 	}
 
 	editExam = (exam: MISExam): void => {
@@ -101,11 +97,10 @@ class Reports extends Component<propsType, S> {
 	}
 
 	createNewExam = (): void => {
-
 		const { section_id } = this.state
 
 		if (section_id === '') {
-			alert("Please select class first!")
+			alert('Please select class first!')
 			return
 		}
 
@@ -117,19 +112,17 @@ class Reports extends Component<propsType, S> {
 	}
 
 	createBulkExams = (): void => {
-		const url = "/reports/bulk-exams"
+		const url = '/reports/bulk-exams'
 		window.location.href = url
 	}
 
 	getClassID = (section_id: string) => {
 		const { classes } = this.props
 
-		return Object.values(classes)
-			.find(c => c.sections[section_id] ? true : false).id
+		return Object.values(classes).find(c => (c.sections[section_id] ? true : false)).id
 	}
 
 	renderClassResultSheet = (section: AugmentedSection) => {
-
 		const { exam_title, year, month, section_id } = this.state
 		const { exams, students, grades, schoolName } = this.props
 
@@ -137,9 +130,12 @@ class Reports extends Component<propsType, S> {
 		let filtered_exams: MISExam[] = []
 
 		for (const exam of Object.values(exams)) {
-
-			if (exam.name === exam_title && moment(exam.date).format("YYYY") === year && exam.section_id === section_id &&
-				(exam_title === "Test" && month ? moment(exam.date).format("MMMM") === month : true)) {
+			if (
+				exam.name === exam_title &&
+				moment(exam.date).format('YYYY') === year &&
+				exam.section_id === section_id &&
+				(exam_title === 'Test' && month ? moment(exam.date).format('MMMM') === month : true)
+			) {
 				filtered_exams.push(exam)
 			}
 		}
@@ -147,7 +143,6 @@ class Reports extends Component<propsType, S> {
 		const exam_students = Object.values(students)
 			.filter(student => student && student.Name && student.section_id && student.exams)
 			.reduce<MergeStudentsExams[]>((agg, curr) => {
-
 				const merge_exams: AugmentedMISExam[] = []
 
 				for (const exam of filtered_exams) {
@@ -157,41 +152,41 @@ class Reports extends Component<propsType, S> {
 					}
 				}
 				// in case there is no exams for the curr student, no need to put into list
-				if (merge_exams.length === 0)
-					return agg
+				if (merge_exams.length === 0) return agg
 
 				return [...agg, { ...curr, merge_exams }]
-
 			}, [])
 
 		const marks_sheet = getStudentExamMarksSheet(exam_students, grades)
 
-		return chunkify(marks_sheet, chunkSize)
-			.map((chunkItems: StudentMarksSheet[], index: number) => <ClassResultSheet key={index}
+		return chunkify(
+			marks_sheet,
+			chunkSize
+		).map((chunkItems: StudentMarksSheet[], index: number) => (
+			<ClassResultSheet
+				key={index}
 				sectionName={section ? section.namespaced_name : ''}
 				relevant_exams={filtered_exams}
 				examName={exam_title}
 				schoolName={schoolName}
 				students={chunkItems}
-				chunkSize={index === 0 ? 0 : chunkSize * index} />)
-
+				chunkSize={index === 0 ? 0 : chunkSize * index}
+			/>
+		))
 	}
 
 	onStateChange = () => {
-
 		const { section_id, exam_title, year, month } = this.state
 
 		const url = '/reports'
 		let params = `section_id=${section_id}&exam_title=${exam_title}&year=${year}`
 
-		if (exam_title === "Test")
-			params = params + `&month=${month}`
+		if (exam_title === 'Test') params = params + `&month=${month}`
 
-		window.history.replaceState(this.state, "Grade Book", `${url}?${params}`)
+		window.history.replaceState(this.state, 'Grade Book', `${url}?${params}`)
 	}
 
 	UNSAFE_componentWillReceiveProps(nextProps: propsType) {
-
 		const parsed_query = queryString.parse(nextProps.location.search)
 
 		const section_id = parsed_query.section_id ? parsed_query.section_id.toString() : ''
@@ -208,12 +203,12 @@ class Reports extends Component<propsType, S> {
 	}
 
 	render() {
-
 		const { section_id, exam_title, year, month } = this.state
 		const { classes, exams } = this.props
 
-		const sections = getSectionsFromClasses(classes)
-			.sort((a, b) => (a.classYear || 0) - (b.classYear || 0))
+		const sections = getSectionsFromClasses(classes).sort(
+			(a, b) => (a.classYear || 0) - (b.classYear || 0)
+		)
 
 		const curr_section = sections.find(section => section.id === section_id)
 
@@ -221,119 +216,182 @@ class Reports extends Component<propsType, S> {
 		let years = new Set<string>()
 
 		for (const [, exam] of Object.entries(exams)) {
+			years.add(moment(exam.date).format('YYYY'))
 
-			years.add(moment(exam.date).format("YYYY"))
-
-			if (exam
-				&& exam.id
-				&& exam.section_id === section_id
-				&& moment(exam.date).format("YYYY") === year
-				&& (exam_title === "Test" && month !== "" ? moment(exam.date).format("MMMM") === month : true)) {
+			if (
+				exam &&
+				exam.id &&
+				exam.section_id === section_id &&
+				moment(exam.date).format('YYYY') === year &&
+				(exam_title === 'Test' && month !== ''
+					? moment(exam.date).format('MMMM') === month
+					: true)
+			) {
 				filtered_exams.push(exam)
 			}
 		}
 
-		return <Layout history={this.props.history}>
-			<div className="reports-page no-print">
-				<div className="title">Grade Book</div>
-				<div className="form section exams-filter">
-					<div className="mis-table">
-						<div className="row">
-							<label>Class/Section</label>
-							<select {...this.former.super_handle(["section_id"], () => true, this.onStateChange)}>
-								<option value="">Select Section</option>
-								{
-									sections.map(section => <option key={section.id} value={section.id}>{section.namespaced_name}</option>)
-								}
-							</select>
-						</div>
-						<div className="row">
-							<label>Exams for Year</label>
-							<select {...this.former.super_handle(["year"], () => true, this.onStateChange)}>
-								<option value="">Select Year</option>
-								{
-									Array.from(years)
-										.sort((a, b) => parseInt(b) - parseInt(a))
-										.map(year => <option key={year} value={year}>{year}</option>)
-								}
-							</select>
-						</div>
-						<div className="row">
-							<label>Exam Title</label>
-							<select {...this.former.super_handle(["exam_title"], () => true, this.onStateChange)}>
-								<option value="">Select Exam</option>
-								{
-									ExamTitles.map(title => <option key={title} value={title}>{title}</option>)
-								}
-							</select>
-						</div>
-						{
-							this.state.exam_title === "Test" && <div className="row">
-								<label>Test Month</label>
-								<select {...this.former.super_handle(["month"], () => true, this.onStateChange)}>
-									<option value="">Select Month</option>
-									{
-										months.map(month => <option key={month} value={month}>{month}</option>)
-									}
+		return (
+			<Layout history={this.props.history}>
+				<div className="reports-page no-print">
+					<div className="title">Grade Book</div>
+					<div className="form section exams-filter">
+						<div className="mis-table">
+							<div className="row">
+								<label>Class/Section</label>
+								<select
+									{...this.former.super_handle(
+										['section_id'],
+										() => true,
+										this.onStateChange
+									)}>
+									<option value="">Select Section</option>
+									{sections.map(section => (
+										<option key={section.id} value={section.id}>
+											{section.namespaced_name}
+										</option>
+									))}
 								</select>
 							</div>
-						}
-					</div>
-					<div className="create-exam">
-						<div className="button blue" onClick={() => this.createNewExam()}>Create New Exam</div>
-						<div className="button grey create-exam" onClick={this.createBulkExams}>Create Bulk Exams</div>
-					</div>
-				</div>
-				{
-					exam_title && year && <div className="section exams-list">
-						<fieldset>
-							<legend>{exam_title.toUpperCase()}</legend>
-							<div className="exams-table">
-								<div className="table-row table-header">
-									<div className="thead cell">Subject</div>
-									<div className="thead cell">Max Score</div>
-									<div className="thead cell">Date</div>
-									<div className="thead cell" style={{ width: "10%" }}>Edit/Delete</div>
-								</div>
-								{
-									filtered_exams
-										.filter(exam => exam.name === exam_title)
-										.sort((a, b) => a.subject.localeCompare(b.subject))
-										.map(exam => <div className="table-row" key={exam.id}>
-											<div className="cell">
-												<Link to={`/reports/${exam.class_id}/${exam.section_id}/exam/${exam.id}`}>{exam.subject}</Link>
-											</div>
-											<div className="cell">{exam.total_score}</div>
-											<div className="cell">{moment(exam.date).format("DD/MM")}</div>
-											<div className="cell" style={{ width: "10%" }}>
-												<div className="">
-													<img className="edit-icon" src={EditIcon} onClick={() => this.editExam(exam)} alt="edit" />
-													<img className="delete-icon" src={DeleteIcon} onClick={() => this.deleteExam(exam.id)} alt="delete" />
-												</div>
-											</div>
-										</div>)
-								}
+							<div className="row">
+								<label>Exams for Year</label>
+								<select
+									{...this.former.super_handle(
+										['year'],
+										() => true,
+										this.onStateChange
+									)}>
+									<option value="">Select Year</option>
+									{Array.from(years)
+										.sort((a, b) => parseInt(b) - parseInt(a))
+										.map(year => (
+											<option key={year} value={year}>
+												{year}
+											</option>
+										))}
+								</select>
 							</div>
-						</fieldset>
-						<div className="row">
-							<div className="button grey" onClick={() => window.print()} style={{ marginRight: 5 }}>Print Result Sheet</div>
+							<div className="row">
+								<label>Exam Title</label>
+								<select
+									{...this.former.super_handle(
+										['exam_title'],
+										() => true,
+										this.onStateChange
+									)}>
+									<option value="">Select Exam</option>
+									{ExamTitles.map(title => (
+										<option key={title} value={title}>
+											{title}
+										</option>
+									))}
+								</select>
+							</div>
+							{this.state.exam_title === 'Test' && (
+								<div className="row">
+									<label>Test Month</label>
+									<select
+										{...this.former.super_handle(
+											['month'],
+											() => true,
+											this.onStateChange
+										)}>
+										<option value="">Select Month</option>
+										{months.map(month => (
+											<option key={month} value={month}>
+												{month}
+											</option>
+										))}
+									</select>
+								</div>
+							)}
+						</div>
+						<div className="create-exam">
+							<div className="button blue" onClick={() => this.createNewExam()}>
+								Create New Exam
+							</div>
+							<div className="button grey create-exam" onClick={this.createBulkExams}>
+								Create Bulk Exams
+							</div>
 						</div>
 					</div>
-				}
-			</div>
-			{
-				section_id && exam_title && year && <div className="print-only">{this.renderClassResultSheet(curr_section)}</div>
-			}
-		</Layout>
+					{exam_title && year && (
+						<div className="section exams-list">
+							<fieldset>
+								<legend>{exam_title.toUpperCase()}</legend>
+								<div className="exams-table">
+									<div className="mis-table-row table-header">
+										<div className="thead cell">Subject</div>
+										<div className="thead cell">Max Score</div>
+										<div className="thead cell">Date</div>
+										<div className="thead cell" style={{ width: '10%' }}>
+											Edit/Delete
+										</div>
+									</div>
+									{filtered_exams
+										.filter(exam => exam.name === exam_title)
+										.sort((a, b) => a.subject.localeCompare(b.subject))
+										.map(exam => (
+											<div className="mis-table-row" key={exam.id}>
+												<div className="cell">
+													<Link
+														to={`/reports/${exam.class_id}/${exam.section_id}/exam/${exam.id}`}>
+														{exam.subject}
+													</Link>
+												</div>
+												<div className="cell">{exam.total_score}</div>
+												<div className="cell">
+													{moment(exam.date).format('DD/MM')}
+												</div>
+												<div className="cell" style={{ width: '10%' }}>
+													<div className="">
+														<img
+															className="edit-icon"
+															src={EditIcon}
+															onClick={() => this.editExam(exam)}
+															alt="edit"
+														/>
+														<img
+															className="delete-icon"
+															src={DeleteIcon}
+															onClick={() => this.deleteExam(exam.id)}
+															alt="delete"
+														/>
+													</div>
+												</div>
+											</div>
+										))}
+								</div>
+							</fieldset>
+							<div className="row">
+								<div
+									className="button grey"
+									onClick={() => window.print()}
+									style={{ marginRight: 5 }}>
+									Print Result Sheet
+								</div>
+							</div>
+						</div>
+					)}
+				</div>
+				{section_id && exam_title && year && (
+					<div className="print-only">{this.renderClassResultSheet(curr_section)}</div>
+				)}
+			</Layout>
+		)
 	}
 }
 
-export default connect((state: RootReducerState) => ({
-	classes: state.db.classes,
-	students: state.db.students,
-	exams: state.db.exams,
-	grades: state.db.settings.exams.grades,
-	schoolName: state.db.settings.schoolName
-}), (dispatch: Function) => ({
-	deleteExam: (students: Array<string>, exam_id: string) => dispatch(deleteExam(students, exam_id))
-}))(Reports)
+export default connect(
+	(state: RootReducerState) => ({
+		classes: state.db.classes,
+		students: state.db.students,
+		exams: state.db.exams,
+		grades: state.db.settings.exams.grades,
+		schoolName: state.db.settings.schoolName
+	}),
+	(dispatch: Function) => ({
+		deleteExam: (students: Array<string>, exam_id: string) =>
+			dispatch(deleteExam(students, exam_id))
+	})
+)(Reports)
