bc4afb4a2cce2c61623989fa251876000801ed45
Mudassar Ali <Mudassaralics007@gmail.com>
2021-04-21 20:57:12 +0500

    fix mobile menu click issue and refactor

--- web/src/components/navbar/index.tsx
+++ web/src/components/navbar/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import clsx from 'clsx'
 import { Menu, Transition } from '@headlessui/react'
 import { Link, useHistory, useLocation } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
+import { ArrowLeftIcon } from '@heroicons/react/outline'
+
 import { createLogout } from 'actions'
-import clsx from 'clsx'
 
 interface AppHeaderProps {
 	title?: string
@@ -14,10 +16,7 @@ export const AppHeader = ({ title }: AppHeaderProps) => {
 	const location = useLocation()
 	const history = useHistory()
 
-	const {
-		auth,
-		db: { onboarding, faculty }
-	} = useSelector((state: RootReducerState) => state)
+	const { auth } = useSelector((state: RootReducerState) => state)
 
 	const isUserLogged = auth?.token && auth?.faculty_id
 
@@ -38,7 +37,7 @@ export const AppHeader = ({ title }: AppHeaderProps) => {
 				})}>
 				<Menu>
 					{({ open: openMenu }: { open: boolean }) => (
-						<div className="flex flex-col max-w-screen-3xl mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-16">
+						<div className="flex flex-col mx-auto max-w-screen-3xl md:items-center md:justify-between md:flex-row md:px-6 lg:px-16">
 							<div
 								className={clsx('flex flex-row items-center justify-between p-3', {
 									'w-full': isUserLogged
@@ -47,24 +46,14 @@ export const AppHeader = ({ title }: AppHeaderProps) => {
 									location.pathname !== '/home' &&
 									location.pathname !== '/' ? (
 									<div
-										className="focus:shadow-outline text-red-brand rounded-full shadow-sm p-2 border border-gray-200 bg-white cursor-pointer"
+										className="p-2 bg-white border border-gray-200 rounded-full shadow-sm cursor-pointer focus:shadow-outline text-red-brand"
 										onClick={() => history.goBack()}>
-										<svg
-											className="w-6"
-											xmlns="http://www.w3.org/2000/svg"
-											viewBox="0 0 20 20"
-											fill="currentColor">
-											<path
-												fillRule="evenodd"
-												d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
-												clipRule="evenodd"
-											/>
-										</svg>
+										<ArrowLeftIcon className="w-6" />
 									</div>
 								) : (
 									<Link to={isUserLogged ? '/home' : '/'}>
 										<img
-											className="image h-10 w-10"
+											className="w-10 h-10 image"
 											src="/favicon.ico"
 											alt="brand-logo"
 										/>
@@ -72,7 +61,7 @@ export const AppHeader = ({ title }: AppHeaderProps) => {
 								)}
 
 								{title && (
-									<div className="text-white font-semibold text-lg">{title}</div>
+									<div className="text-lg font-semibold text-white">{title}</div>
 								)}
 
 								<Menu.Button
@@ -113,7 +102,7 @@ export const AppHeader = ({ title }: AppHeaderProps) => {
 										leaveTo="transform opacity-0 scale-95">
 										<Menu.Items
 											static
-											className="absolute right-2 w-44 -top-2 md:top-6 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none z-50">
+											className="absolute z-50 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none right-2 w-44 -top-2 md:top-6">
 											<div className="py-1">
 												<Menu.Item>
 													<Link
@@ -129,7 +118,7 @@ export const AppHeader = ({ title }: AppHeaderProps) => {
 												<Menu.Item>
 													<button
 														onClick={handleLogout}
-														className="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left">
+														className="flex justify-between w-full px-4 py-2 text-sm leading-5 text-left text-gray-700">
 														Logout
 													</button>
 												</Menu.Item>
@@ -138,22 +127,31 @@ export const AppHeader = ({ title }: AppHeaderProps) => {
 									</Transition>
 								</div>
 							) : (
-								<nav
-									className={`${openMenu ? 'flex' : 'hidden'
-										} flex-col flex-grow pb-4 md:pb-0 md:flex md:justify-end md:flex-row flex`}>
+								<Menu.Items
+									static
+									className={clsx(
+										'flex-col flex-grow pb-4 md:pb-0 md:flex md:justify-end md:flex-row flex',
+										openMenu ? 'flex' : 'hidden'
+									)}>
 									<div className="relative">
 										<Menu>
 											{({ open }: { open: boolean }) => (
 												<>
 													<Menu.Button
-														className={`${open ? 'bg-gray-200' : ''
-															} hidden md:flex flex-row text-gray-900 bg-gray-100 items-center w-full px-4 py-2 mt-2 text-left bg-transparent rounded-lg  md:w-auto md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:outline-none focus:shadow-outline`}>
+														className={clsx(
+															'hidden md:flex flex-row text-gray-900 bg-gray-100 items-center w-full px-4 py-2 mt-2 text-left bg-transparent rounded-lg  md:w-auto md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:outline-none focus:shadow-outline',
+															{
+																'bg-gray-200': open
+															}
+														)}>
 														<span>Why Us?</span>
 														<svg
 															fill="currentColor"
 															viewBox="0 0 20 20"
-															className={`${open ? 'rotate-180' : 'rotate-0'
-																} inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1`}>
+															className={clsx(
+																'inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1',
+																open ? 'rotate-180' : 'rotate-0'
+															)}>
 															<path
 																fillRule="evenodd"
 																d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
@@ -162,24 +160,26 @@ export const AppHeader = ({ title }: AppHeaderProps) => {
 														</svg>
 													</Menu.Button>
 													<div
-														className={`${open ? 'block' : 'hidden'
-															} absolute right-0 md:-right-28 w-full md:w-96 mt-3`}>
-														<div className="p-2 bg-teal-400 rounded-md shadow-lg relative">
-															<div className="flex flex-row items-center justify-center text-xs divide-x-2 divide-gray-100 space-x-2">
+														className={clsx(
+															'absolute right-0 md:-right-28 w-full md:w-96 mt-3',
+															open ? 'block' : 'hidden'
+														)}>
+														<div className="relative p-2 bg-teal-400 rounded-md shadow-lg">
+															<div className="flex flex-row items-center justify-center space-x-2 text-xs divide-x-2 divide-gray-100">
 																<a
-																	className="flex row items-start bg-transparent px-2 py-1 text-white focus:outline-none focus:shadow-outline"
+																	className="flex items-start px-2 py-1 text-white bg-transparent row focus:outline-none focus:shadow-outline"
 																	href="#problems">
 																	Problems
 																</a>
 
 																<a
-																	className="flex row items-start bg-transparent px-2 py-1 text-white focus:outline-none focus:shadow-outline"
+																	className="flex items-start px-2 py-1 text-white bg-transparent row focus:outline-none focus:shadow-outline"
 																	href="#testimonials">
 																	Testimonials
 																</a>
 
 																<a
-																	className="flex row items-start bg-transparent px-2 py-1 text-white focus:outline-none focus:shadow-outline"
+																	className="flex items-start px-2 py-1 text-white bg-transparent row focus:outline-none focus:shadow-outline"
 																	href="#customers">
 																	Our Customer
 																</a>
@@ -216,7 +216,7 @@ export const AppHeader = ({ title }: AppHeaderProps) => {
 										className="px-4 py-2 mt-2 bg-transparent rounded-lg md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:shadow-outline">
 										Login
 									</Link>
-								</nav>
+								</Menu.Items>
 							)}
 						</div>
 					)}
