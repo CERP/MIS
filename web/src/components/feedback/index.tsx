f5be8831932334cd5f6b0f33037ed943681e26b0
Mudassar Ali <Mudassaralics007@gmail.com>
2021-05-05 12:08:06 +0500

    style and logic tweaks

--- web/src/components/feedback/index.tsx
+++ web/src/components/feedback/index.tsx
@@ -1,28 +1,28 @@
 import React, { useState } from 'react'
 import clsx from 'clsx'
-
+import { Transition } from '@headlessui/react'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
 import { feedbacks } from 'constants/feedback'
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
 
 export const CustomerFeedback = () => {
 	const [index, setIndex] = useState(0)
 
-	// useLayoutEffect(() => {
-	// 	setTimeout(() => {
-	// 		const currIndex = index < feedbacks.length - 1 ? index + 1 : 0
-	// 		setIndex(currIndex)
-	// 	}, 5000)
-	// }, [index])
-
 	const feedback = feedbacks[index]
 
-	const nextIndex = index + 1
-	const prevIndex = index - 1
-
 	return (
-		<div className="w-full md:w-2/3 mt-20 md:mt-0 px-10 md:px-20">
+		<Transition
+			enter="transition-opacity duration-75"
+			enterFrom="opacity-0"
+			enterTo="opacity-100"
+			leave="transition-opacity duration-150"
+			leaveFrom="opacity-100"
+			leaveTo="opacity-0"
+			as={'div'}
+			appear={true}
+			show={true}
+			className="w-full md:w-2/3 mt-20 md:mt-0 px-10 md:px-20">
 			<div className="relative">
-				<div className="h-96 bg-red-brand rounded-xl text-center text-white">
+				<div className="h-96 bg-red-brand rounded-xl text-center text-white shadow-lg">
 					<div className="pt-28">
 						<div className="font-bold">{feedback.name}</div>
 						<div>
@@ -53,18 +53,14 @@ export const CustomerFeedback = () => {
 					/>
 				</div>
 				<ChevronRightIcon
-					onClick={
-						nextIndex <= feedbacks.length - 1 ? () => setIndex(nextIndex) : undefined
-					}
-					className="hidden md:block text-white bg-red-brand hover:text-red-brand hover:bg-gray-100 w-14 cursor-pointer rounded-full p-2 inset-y-40 -right-40 absolute"
+					onClick={index < feedbacks.length - 1 ? () => setIndex(index + 1) : undefined}
+					className="hidden md:block text-white bg-red-brand hover:text-red-brand hover:bg-gray-100 w-14 cursor-pointer rounded-full p-1 inset-y-40 -right-40 absolute"
 				/>
 				<ChevronLeftIcon
-					onClick={
-						nextIndex >= feedbacks.length - 1 ? () => setIndex(prevIndex) : undefined
-					}
-					className="hidden md:block text-white bg-red-brand hover:text-red-brand hover:bg-gray-100 w-14 cursor-pointer rounded-full p-2 inset-y-40 -left-40 absolute"
+					onClick={index > 0 ? () => setIndex(index - 1) : undefined}
+					className="hidden md:block text-white bg-red-brand hover:text-red-brand hover:bg-gray-100 w-14 cursor-pointer rounded-full p-1 inset-y-40 -left-40 absolute"
 				/>
 			</div>
-		</div>
+		</Transition>
 	)
 }
