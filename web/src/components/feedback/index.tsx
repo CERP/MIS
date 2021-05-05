c3ac0b11bae51dbaf12e92575fcc94f690009577
Mudassar Ali <Mudassaralics007@gmail.com>
2021-05-05 05:41:16 +0500

    add prev and next button to show testimonial

--- web/src/components/feedback/index.tsx
+++ web/src/components/feedback/index.tsx
@@ -1,19 +1,23 @@
-import React, { useLayoutEffect, useState } from 'react'
+import React, { useState } from 'react'
 import clsx from 'clsx'
 
 import { feedbacks } from 'constants/feedback'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
 
 export const CustomerFeedback = () => {
 	const [index, setIndex] = useState(0)
 
-	useLayoutEffect(() => {
-		setTimeout(() => {
-			const currIndex = index < feedbacks.length - 1 ? index + 1 : 0
-			setIndex(currIndex)
-		}, 5000)
-	}, [index])
+	// useLayoutEffect(() => {
+	// 	setTimeout(() => {
+	// 		const currIndex = index < feedbacks.length - 1 ? index + 1 : 0
+	// 		setIndex(currIndex)
+	// 	}, 5000)
+	// }, [index])
 
 	const feedback = feedbacks[index]
+
+	const nextIndex = index + 1
+	const prevIndex = index - 1
 
 	return (
 		<div className="w-full md:w-2/3 mt-20 md:mt-0 px-10 md:px-20">
@@ -29,11 +33,12 @@ export const CustomerFeedback = () => {
 						{feedback.body}
 					</p>
 					<div className="flex justify-center items-center mt-10 space-x-2">
-						{[0, 1, 2].map(v => (
+						{[...new Array(feedbacks.length).keys()].map(v => (
 							<div
 								key={v + feedback.name}
+								onClick={() => setIndex(v)}
 								className={clsx(
-									'h-4 w-4 rounded-full',
+									'h-5 w-5 rounded-full cursor-pointer',
 									v === index ? 'bg-teal-brand' : 'bg-white'
 								)}
 							/>
@@ -47,6 +52,18 @@ export const CustomerFeedback = () => {
 						alt={feedback.name}
 					/>
 				</div>
+				<ChevronRightIcon
+					onClick={
+						nextIndex <= feedbacks.length - 1 ? () => setIndex(nextIndex) : undefined
+					}
+					className="hidden md:block text-white bg-red-brand hover:text-red-brand hover:bg-gray-100 w-14 cursor-pointer rounded-full p-2 inset-y-40 -right-40 absolute"
+				/>
+				<ChevronLeftIcon
+					onClick={
+						nextIndex >= feedbacks.length - 1 ? () => setIndex(prevIndex) : undefined
+					}
+					className="hidden md:block text-white bg-red-brand hover:text-red-brand hover:bg-gray-100 w-14 cursor-pointer rounded-full p-2 inset-y-40 -left-40 absolute"
+				/>
 			</div>
 		</div>
 	)
