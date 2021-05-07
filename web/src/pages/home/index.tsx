f2e9f988a081a17b4cc9a7e02de7811c749d897c
hamzaahmed00 <78449376+hamzaahmed00@users.noreply.github.com>
2021-05-07 17:47:21 +0500

    Desktop resdesigned (#336)
    * Desktop resdesigned
    
    * replace inline-svg with import
    
    * use react-media-hook
    
    * style tweaks, make responsive svg using react-media-hook and cond, remove duplicate code
    
    Co-authored-by: Mudassar Ali <Mudassaralics007@gmail.com>

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import cond from 'cond-construct'
+import { useMediaPredicate } from 'react-media-hook'
 
 import { SettingsTab } from './settings'
 import { ActionTab } from './actions'
@@ -31,6 +32,7 @@ const TabbarContent = [
 
 export const Home = () => {
 	const [activeTab, setActiveTab] = useState(Tabs.ACTIONS)
+	const biggerThan880 = useMediaPredicate('(min-width: 880px)')
 
 	const renderComponent = () =>
 		cond([
@@ -39,10 +41,34 @@ export const Home = () => {
 			[activeTab === Tabs.STATS, () => <StatsTab />]
 		])
 
+	if (!biggerThan880) {
+		return (
+			<AppLayout title={'Home' + ' - ' + TabbarContent[activeTab].title}>
+				<Tabbar tab={activeTab} setTab={setActiveTab} content={TabbarContent} />
+				{renderComponent()}
+			</AppLayout>
+		)
+	}
+
 	return (
-		<AppLayout title={'Home' + ' - ' + TabbarContent[activeTab].title}>
-			<Tabbar tab={activeTab} setTab={setActiveTab} content={TabbarContent} />
-			{renderComponent()}
+		<AppLayout>
+			<div className="grid grid-cols-3 relative">
+				<div className="fixed w-4/12">
+					<div className="flex flex-col items-center bg-white shadow-md rounded-2xl m-5 border-gray-100 border">
+						<h1 className="text-xl font-semibold mt-4">Settings</h1>
+						<SettingsTab />
+					</div>
+				</div>
+				<div className="col-start-2 col-end-3">
+					<div className="flex flex-col items-center rounded-2xl m-5">
+						<h1 className="text-xl font-semibold">Actions</h1>
+						<ActionTab />
+					</div>
+				</div>
+				<div className="fixed right-0 w-4/12">
+					<StatsTab />
+				</div>
+			</div>
 		</AppLayout>
 	)
 }
