5219193a0351c493a2edf98ba91d9d5322e915f5
Mudassar Ali <Mudassaralics007@gmail.com>
2021-05-07 16:36:48 +0500

    use react-media-hook

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import cond from 'cond-construct'
+import { useMediaPredicate } from 'react-media-hook'
 
 import { SettingsTab } from './settings'
 import { ActionTab } from './actions'
@@ -7,7 +8,6 @@ import { StatsTab } from './statistics'
 
 import { AppLayout } from 'components/Layout/appLayout'
 import { Tabbar } from 'components/tabs'
-import { isMobile } from 'utils/helpers'
 
 enum Tabs {
 	SETTINGS,
@@ -32,6 +32,7 @@ const TabbarContent = [
 
 export const Home = () => {
 	const [activeTab, setActiveTab] = useState(Tabs.ACTIONS)
+	const biggerThan880 = useMediaPredicate('(min-width: 880px)')
 
 	const renderComponent = () =>
 		cond([
@@ -40,23 +41,27 @@ export const Home = () => {
 			[activeTab === Tabs.STATS, () => <StatsTab />]
 		])
 
-	return isMobile() ? (
-		<AppLayout title={'Home' + ' - ' + TabbarContent[activeTab].title}>
-			<Tabbar tab={activeTab} setTab={setActiveTab} content={TabbarContent} />
-			{renderComponent()}
-		</AppLayout>
-	) : (
+	if (!biggerThan880) {
+		return (
+			<AppLayout title={'Home' + ' - ' + TabbarContent[activeTab].title}>
+				<Tabbar tab={activeTab} setTab={setActiveTab} content={TabbarContent} />
+				{renderComponent()}
+			</AppLayout>
+		)
+	}
+
+	return (
 		<AppLayout>
 			<div className="grid grid-cols-3 relative">
 				<div className="fixed w-4/12">
-					<div className="flex flex-col items-center bg-white shadow-md rounded-2xl m-5 border-gray-200 border">
-						<h1 className="text-2xl font-bold">Setup</h1>
+					<div className="flex flex-col items-center bg-white shadow-md rounded-2xl m-5 border-gray-100 border">
+						<h1 className="text-xl font-semibold mt-4">Settings</h1>
 						<SettingsTab />
 					</div>
 				</div>
 				<div className="col-start-2 col-end-3">
-					<div className="flex flex-col items-center rounded-2xl m-5 border-gray-600">
-						<h1 className="text-2xl font-bold">Actions</h1>
+					<div className="flex flex-col items-center rounded-2xl m-5">
+						<h1 className="text-xl font-semibold">Actions</h1>
 						<ActionTab />
 					</div>
 				</div>
