ca14b34ae4b4eb3c852efe6d240917819b9db97b
ali ahmad <raoaliahmad03@gmail.com>
2021-05-09 06:34:25 +0500

    student/staff navigation page refactor

--- web/src/pages/staff/index.tsx
+++ web/src/pages/staff/index.tsx
@@ -6,33 +6,32 @@ import { Link } from 'react-router-dom'
 import { CreateOrUpdateStaff } from './create'
 
 const StaffPage = ({ location }: RouteComponentProps) => {
+	// To Add new a new path, update the 'Paths' Object & create a route
+
 	const loc = location.pathname.split('/').slice(-1).pop()
+	const Paths = {
+		Profile: 'profile',
+		Attendance: 'attendance'
+	}
+
 	return (
 		<AppLayout>
 			{loc !== 'new' && (
 				<div className="flex flex-row items-center my-2 w-full justify-center flex-wrap print:hidden">
-					<Link
-						to="profile"
-						className={clsx(
-							'rounded-full px-4 py-2 m-2 text-md border shadow-md md:text-xl',
-							loc === 'profile'
-								? 'bg-teal-brand text-white'
-								: 'bg-white text-teal-brand'
-						)}
-						replace={true}>
-						Profile
-					</Link>
-					<Link
-						to="attendance"
-						className={clsx(
-							'rounded-full px-4 py-2 m-2 text-md border shadow-md md:text-xl',
-							loc === 'attendance'
-								? 'bg-teal-brand text-white'
-								: 'bg-white text-teal-brand'
-						)}
-						replace={true}>
-						Attendance
-					</Link>
+					{Object.entries(Paths).map(([title, path]) => (
+						<Link
+							key={path}
+							to={path}
+							className={clsx(
+								'rounded-full px-4 py-2 m-2 text-md border shadow-md md:text-xl',
+								loc === path
+									? 'bg-teal-brand text-white'
+									: 'bg-white text-teal-brand'
+							)}
+							replace={true}>
+							{title}
+						</Link>
+					))}
 				</div>
 			)}
 			<Route path="/staff/new">
--- web/src/pages/students/index.tsx
+++ web/src/pages/students/index.tsx
@@ -7,66 +7,35 @@ import { Link } from 'react-router-dom'
 import { CreateOrUpdateStudent } from './add'
 
 const StudentPage = ({ location }: RouteComponentProps) => {
+	// To Add new a new path, update the 'Paths' Object & create a route
+
 	const loc = location.pathname.split('/').slice(-1).pop()
+	const Paths = {
+		Profile: 'profile',
+		Payments: 'payments',
+		Grades: 'grades',
+		Attendance: 'attendance',
+		Certificates: 'certificates'
+	}
+
 	return (
 		<AppLayout>
 			{loc !== 'new' && (
 				<div className="flex flex-row items-center my-2 w-full justify-center flex-wrap print:hidden">
-					<Link
-						to="profile"
-						className={clsx(
-							'rounded-full px-4 py-2 m-2 text-md border shadow-md md:text-xl',
-							loc === 'profile'
-								? 'bg-teal-brand text-white'
-								: 'bg-white text-teal-brand'
-						)}
-						replace={true}>
-						Profile
-					</Link>
-					<Link
-						to="payments"
-						className={clsx(
-							'rounded-full px-4 py-2 m-2 text-md border shadow-md md:text-xl',
-							loc === 'payments'
-								? 'bg-teal-brand text-white'
-								: 'bg-white text-teal-brand'
-						)}
-						replace={true}>
-						Payments
-					</Link>
-					<Link
-						to="grades"
-						className={clsx(
-							'rounded-full px-4 py-2 m-2 text-md border shadow-md md:text-xl',
-							loc === 'grades'
-								? 'bg-teal-brand text-white'
-								: 'bg-white text-teal-brand'
-						)}
-						replace={true}>
-						Grades
-					</Link>
-					<Link
-						to="attendance"
-						className={clsx(
-							'rounded-full px-4 py-2 m-2 text-md border shadow-md md:text-xl',
-							loc === 'attendance'
-								? 'bg-teal-brand text-white'
-								: 'bg-white text-teal-brand'
-						)}
-						replace={true}>
-						Attendance
-					</Link>
-					<Link
-						to="certificates"
-						className={clsx(
-							'rounded-full px-4 py-2 m-2 text-md border shadow-md md:text-xl',
-							loc === 'certificates'
-								? 'bg-teal-brand text-white'
-								: 'bg-white text-teal-brand'
-						)}
-						replace={true}>
-						Certificates
-					</Link>
+					{Object.entries(Paths).map(([title, path]) => (
+						<Link
+							key={path}
+							to={path}
+							className={clsx(
+								'rounded-full px-4 py-2 m-2 text-md border shadow-md md:text-xl',
+								loc === path
+									? 'bg-teal-brand text-white'
+									: 'bg-white text-teal-brand'
+							)}
+							replace={true}>
+							{title}
+						</Link>
+					))}
 				</div>
 			)}
 			<Route path="/students/new">
