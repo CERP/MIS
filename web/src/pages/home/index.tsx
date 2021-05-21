eaf76d0ab4d35a022589534dc89162311129a572
Hamza <hamzaahmed00@hotmail.com>
2021-05-21 18:29:08 +0500

    Added Restriced routing based on permissions

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -8,6 +8,7 @@ import { StatsTab } from './statistics'
 
 import { AppLayout } from 'components/Layout/appLayout'
 import { Tabbar } from 'components/tabs'
+import { useSelector } from 'react-redux'
 
 enum Tabs {
 	SETTINGS,
@@ -34,11 +35,25 @@ export const Home = () => {
 	const [activeTab, setActiveTab] = useState(Tabs.ACTIONS)
 	const biggerThan880 = useMediaPredicate('(min-width: 880px)')
 
+	const { faculty } = useSelector((state: RootReducerState) => state.db)
+
+	const { faculty_id } = useSelector((state: RootReducerState) => state.auth)
+	const { permissions, Admin, SubAdmin } = faculty[faculty_id]
+
 	const renderComponent = () =>
 		cond([
-			[activeTab === Tabs.SETTINGS, () => <SettingsTab />],
-			[activeTab === Tabs.ACTIONS, () => <ActionTab />],
-			[activeTab === Tabs.STATS, () => <StatsTab />]
+			[
+				activeTab === Tabs.SETTINGS,
+				() => <SettingsTab permissions={permissions} admin={Admin} subAdmin={SubAdmin} />
+			],
+			[
+				activeTab === Tabs.ACTIONS,
+				() => <ActionTab permissions={permissions} admin={Admin} subAdmin={SubAdmin} />
+			],
+			[
+				activeTab === Tabs.STATS,
+				() => <StatsTab permissions={permissions} admin={Admin} subAdmin={SubAdmin} />
+			]
 		])
 
 	if (!biggerThan880) {
@@ -56,17 +71,17 @@ export const Home = () => {
 				<div className="fixed w-4/12">
 					<div className="flex flex-col items-center bg-white shadow-md rounded-2xl m-5 border-gray-100 border">
 						<h1 className="text-xl font-semibold mt-4">Settings</h1>
-						<SettingsTab />
+						<SettingsTab permissions={permissions} admin={Admin} subAdmin={SubAdmin} />
 					</div>
 				</div>
 				<div className="col-start-2 col-end-3">
 					<div className="flex flex-col items-center rounded-2xl m-5">
 						<h1 className="text-xl font-semibold">Actions</h1>
-						<ActionTab />
+						<ActionTab permissions={permissions} admin={Admin} subAdmin={SubAdmin} />
 					</div>
 				</div>
 				<div className="fixed right-0 w-4/12">
-					<StatsTab />
+					<StatsTab permissions={permissions} admin={Admin} subAdmin={SubAdmin} />
 				</div>
 			</div>
 		</AppLayout>
--- web/src/pages/home/settings.tsx
+++ web/src/pages/home/settings.tsx
@@ -9,7 +9,20 @@ import iconTeachers from './assets/teachers.svg'
 import iconStudents from './assets/students.svg'
 import iconFamily from './assets/family.png'
 
-export const SettingsTab = () => {
+type PropTypes = {
+	permissions: {
+		fee: boolean
+		dailyStats: boolean
+		setupPage: boolean
+		expense: boolean
+		family: boolean
+		prospective: boolean
+	}
+	admin: boolean
+	subAdmin: boolean
+}
+
+export const SettingsTab = ({ permissions, admin, subAdmin }: PropTypes) => {
 	const { faculty, students, classes } = useSelector((state: RootReducerState) => state.db)
 
 	const totalStaff = Object.values(faculty).filter(f => isValidTeacher(f) && f.Active).length
@@ -18,37 +31,43 @@ export const SettingsTab = () => {
 		Object.values(students).filter(s => isValidStudent(s) && s.Active)
 	)
 
+	const setupPermission = admin || (subAdmin && permissions.setupPage)
+
 	return (
 		<div className="p-6 lg:w-full">
 			<div className="text-center text-lg mb-6 md:hidden">Tap to configure the module</div>
 			<div className="space-y-4">
 				<Card
 					title={'Staff'}
-					link="/staff"
+					link={setupPermission ? '/staff' : '#'}
 					caption={`Total = ${totalStaff}`}
 					icon={iconTeachers}
 				/>
 				<Card
 					title={'Classes'}
-					link="/classes"
+					link={setupPermission ? '/classes' : '#'}
 					caption={`Total = ${Object.keys(classes).length}`}
 					icon={iconClasses}
+					disabled={!setupPermission}
 				/>
 				<Card
 					title={'Students'}
-					link="/students"
+					link={setupPermission ? '/students' : '#'}
 					caption={`Total = ${totalStudents}`}
+					disabled={!setupPermission}
 					icon={iconStudents}
 				/>
 				<Card
 					title={'Families'}
-					link="/families"
+					link={admin || (subAdmin && permissions.family) ? '/families' : '#'}
+					disabled={!admin || (subAdmin && permissions.family)}
 					caption={`Total = ${totalFamilies}`}
 					icon={iconFamily}
 				/>
 				<Card
 					title={'Profile'}
-					link="/settings"
+					link={setupPermission ? '/settings' : '#'}
+					disabled={!setupPermission}
 					caption={'Profile, Grades, Data etc'}
 					icon={iconStudents}
 				/>
