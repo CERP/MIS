0dd0fbe05af43867adef96d1133f5e4bb0c1f137
Mudassar Ali <Mudassaralics007@gmail.com>
2021-05-31 12:30:35 +0500

    add shadow

--- web/src/components/Button/add-sticky.tsx
+++ web/src/components/Button/add-sticky.tsx
@@ -11,7 +11,7 @@ export const AddStickyButton = ({ label, handleClick }: AddStickyButtonProps) =>
 	return (
 		<div
 			onClick={handleClick}
-			className="flex items-center justify-between fixed z-50 bottom-4 right-4 md:right-8 rounded-full bg-teal-brand md:w-1/4 text-white py-3 px-6 w-11/12 text-lg mr-0.5">
+			className="flex items-center justify-between fixed z-50 bottom-4 right-4 md:right-8 rounded-full bg-teal-brand md:w-1/4 text-white py-3 px-6 w-11/12 text-lg mr-0.5 shadow-md">
 			<div>{label}</div>
 			<PlusIcon className="w-6" />
 		</div>
