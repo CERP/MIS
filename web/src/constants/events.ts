5faab8c5b4e29c50349eac1e52a9db3ac594a07c
Mudassar Ali <Mudassaralics007@gmail.com>
2021-06-06 14:58:58 +0500

    use google storage links for sports images

--- web/src/constants/events.ts
+++ web/src/constants/events.ts
@@ -6,9 +6,9 @@ export const MISEvents: MISEvent[] = [
 		body:
 			'MISchool attended the sports event of a private school association in Faisalabad on 16th November 2019. Around 50 schools were part of this event. We are thankful for the "Association for Private Schools Pakistan (APSP)" and Khalid Hayat Kamoka shb for giving us the opportunity to join their event.',
 		images: [
-			'https://scontent.flhe4-2.fna.fbcdn.net/v/t1.6435-0/s600x600/84527191_116976709861989_6382005167708962816_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=cdbe9c&_nc_eui2=AeE0P2k_3y-QRzqCSl7hUzYjUH2_Mgq_FUNQfb8yCr8VQ7-pRs2rtrWBIEzYPxStyOw8emZxZBPdflOMu7XTwJCK&_nc_ohc=B-8iN70a82UAX9vUmhl&_nc_ht=scontent.flhe4-2.fna&tp=7&oh=443c40b7cfb737b1031a321d9586ccd3&oe=60B73CD9',
-			'https://scontent.flhe4-2.fna.fbcdn.net/v/t1.6435-9/83753539_116978479861812_2875320359442186240_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=cdbe9c&_nc_eui2=AeGzY_-62WQXkB2srATRpBsR0Yjz9NFea1PRiPP00V5rUwl9X_-R-MxW-VyRXfE_dwbfivRjRsL9cALpHclF-JlP&_nc_ohc=mmtEvEzGUx8AX82SZ-a&_nc_ht=scontent.flhe4-2.fna&oh=1eeded35dab2979a4ed51917f9d06115&oe=60B768B3',
-			'https://scontent.flhe4-2.fna.fbcdn.net/v/t1.6435-9/83897108_116979333195060_4266117549701201920_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=cdbe9c&_nc_eui2=AeGSTu4SmjBjyxK_4YARUMEo-U-ShhsDHAj5T5KGGwMcCKuBdQlW6Fsnm5cPaq6PoFU59zp6M0Jl1eseUl1J1G7u&_nc_ohc=ApRr6UqjeLEAX8WAts6&_nc_oc=AQkaO0DkywdZO0PJr4MYxY_6FrRzqiCq7H9331TF4Ir5Yb6EwarNrsypTqiepj8Cq6w&_nc_ht=scontent.flhe4-2.fna&oh=f093ffa2a110c0954b3ee21bada848c8&oe=60B79B84'
+			'https://storage.googleapis.com/mischool/events/sports-festival-2.jpg',
+			'https://storage.googleapis.com/mischool/events/sport-festival-1.jpg',
+			'https://storage.googleapis.com/mischool/events/sports-festival-3.jpg'
 		]
 	},
 	{
@@ -18,9 +18,9 @@ export const MISEvents: MISEvent[] = [
 		body:
 			"MISchool represented in the Ilmxchange Taleemi Mela in Lahore. Ilmxchange is Pakistan's first digital hub for the education services supplier",
 		images: [
-			'https://scontent.flhe4-2.fna.fbcdn.net/v/t1.6435-9/85063526_119718396254487_4354436237814136832_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=cdbe9c&_nc_eui2=AeG6qXHqGB_GRDiwrMmUbK23bByRWBzCN5tsHJFYHMI3m2S683BCXnUeDnJVyK2Di13B1URdsPCxAuid1byeLktJ&_nc_ohc=3pyaN039t3MAX810JmN&_nc_ht=scontent.flhe4-2.fna&oh=0488229d311ce76db32c85ea2d05bc6d&oe=60B66E3C',
-			'https://scontent.flhe4-2.fna.fbcdn.net/v/t1.6435-9/83230973_119722872920706_1406453458449465344_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=cdbe9c&_nc_eui2=AeFZ1aHrOMPWBa_LXwb5jOhqw_IJyhCbth_D8gnKEJu2H9dpMJ2LbH6CkSTtlZPL_o8L8edWRfZg4JTHsp5sUU0X&_nc_ohc=LeDmKO7n7HoAX-CTDZ7&_nc_ht=scontent.flhe4-2.fna&oh=fd5f10a92ea63d4eb8e39a932fab01a6&oe=60B727CB',
-			'https://scontent.flhe4-1.fna.fbcdn.net/v/t1.6435-9/85174790_121213766104950_6886006085107318784_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=cdbe9c&_nc_eui2=AeFnf78uvpi1PqNUIsdXk_CLGDeO_EE2xnoYN478QTbGevcKV98MsJgvKNY3weXU4XkvEurs8w3fW8jo98j4O5as&_nc_ohc=mpLZlwfZUc4AX9Ddsjz&_nc_ht=scontent.flhe4-1.fna&oh=a1267238ea797d7ad2a8a4574336ee83&oe=60B62239'
+			'https://storage.googleapis.com/mischool/events/taleemi-mela-1.jpg',
+			'https://storage.googleapis.com/mischool/events/taleemi-mela-2.jpg',
+			'https://storage.googleapis.com/mischool/events/taleemi-mela-3.jpg'
 		]
 	}
 ]
