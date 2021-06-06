cfd91f03526558c327cab8bd8c0fc8a94972425e
Mudassar Ali <Mudassaralics007@gmail.com>
2021-06-06 15:28:46 +0500

    hard-coding host origin for testing

--- web/src/utils/hostConfig.ts
+++ web/src/utils/hostConfig.ts
@@ -1,5 +1,5 @@
 //@ts-ignore
-export const hostOrigin = window.api_url || '73ad9fcc4162.ngrok.io'
+export const hostOrigin = 'socket.ilmexchange.com'
 
 export const hostWSS = 'wss://' + hostOrigin + '/ws'
 export const hostHTTPS = 'https://' + hostOrigin
