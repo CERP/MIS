01b8669ecf4a090b81900aa3a6456df55227bd21
Mudassar Ali <Mudassaralics007@gmail.com>
2021-06-07 20:20:49 +0500

    change object-contain to object-fit

--- web/src/components/image/index.tsx
+++ web/src/components/image/index.tsx
@@ -68,7 +68,7 @@ export const UploadImage = ({
 				</TModal>
 			)}
 			<img
-				className="w-24 h-24 rounded-full object-contain ring-1 bg-gray-500"
+				className="w-24 h-24 rounded-full ring-1 bg-gray-500 object-fit"
 				src={src || UserIconSvg}
 				alt="image"
 			/>
