da67cefe6a388fd8cb5050fde1583b530c014771
Mudassar Ali <Mudassaralics007@gmail.com>
2021-06-14 02:21:57 +0500

    fix basic design inconsistencies

--- web/src/modules/TIP/index.tsx
+++ web/src/modules/TIP/index.tsx
@@ -55,56 +55,56 @@ const Home: React.FC<PropsType> = ({ faculty, faculty_id }) => {
 			<Headings heading="Welcome to TIP" sub_heading="What would you like to do today ?" />
 			{
 				<Link
-					className={`flex justify-center content-center w-full mt-2 no-underline`}
+					className={`flex justify-center content-center w-full my-2 no-underline`}
 					to={'/targeted-instruction/detailed-analysis'}>
-					<button className="border-none bg-sea-green-tip-brand text-white shadow-lg p-2 px-5 rounded-2xl outline-none cursor-pointer">
+					<button className="border-none bg-sea-green-tip-brand text-white shadow-lg py-2 px-4 rounded-2xl outline-none cursor-pointer">
 						View Class / Groups
 					</button>
 				</Link>
 			}
 			{layout === Layouts.DIAGNOSTIC && (
-				<div className="w-full flex justify-center">
+				<div className="w-full flex justify-center mx-3">
 					<Link
-						className="container sm:px-8 bg-white rounded-2xl m-3 h-44 flex flex-col content-center items-center shadow-lg no-underline"
+						className="w-full bg-white rounded-2xl h-44 flex flex-col content-center items-center shadow-lg no-underline"
 						to={'/targeted-instruction/diagnostic-test'}>
-						<img className="h-24 py-4 w-24" src={DiagnosticItalic} alt="img" />
+						<img className="h-24 w-24" src={DiagnosticItalic} alt="img" />
 						<div className="text-blue-900 text-lg font-bold">Starting Test</div>
 					</Link>
 				</div>
 			)}
 			{layout === Layouts.FORMATIVE && (
-				<div className="w-full flex justify-center">
+				<div className="w-full flex justify-center mx-3">
 					<Link
-						className="container sm:px-8 bg-white rounded-2xl m-3 h-44 flex flex-col content-center items-center shadow-lg no-underline"
+						className="w-full bg-white rounded-2xl h-44 flex flex-col content-center items-center shadow-lg no-underline"
 						to={'/targeted-instruction/formative-test'}>
-						<img className="h-24 py-4 pr-4 w-28" src={Formative} alt="img" />
+						<img className="h-24 w-28" src={Formative} alt="img" />
 						<div className="text-blue-900 text-lg font-bold">Midpoint Test</div>
 					</Link>
 				</div>
 			)}
 			{layout === Layouts.SUMMATIVE && (
-				<div className="w-full flex justify-center">
+				<div className="w-full flex justify-center mx-3">
 					<Link
-						className="container sm:px-8 bg-white rounded-2xl m-3 h-44 flex flex-col content-center items-center shadow-lg no-underline"
+						className="w-full bg-white rounded-2xl h-44 flex flex-col content-center items-center shadow-lg no-underline"
 						to={'/targeted-instruction/summative-test'}>
-						<img className="h-24 py-4 pr-4 w-28" src={Summative} alt="img" />
+						<img className="h-24 w-28" src={Summative} alt="img" />
 						<div className="text-blue-900 text-lg font-bold">Final Test</div>
 					</Link>
 				</div>
 			)}
 
-			<div className="flex flex-row content-center items-center justify-center w-full">
+			<div className="flex flex-row content-center items-center justify-center w-full mt-4">
 				<div className="flex flex-row content-center items-center w-full">
 					<Link
-						className="container sm:px-8 bg-white rounded-xl m-3 h-36 flex flex-col content-center items-center shadow-lg no-underline"
+						className="container sm:px-8 bg-white rounded-xl m-3 h-36 flex flex-col content-center items-center shadow-lg space-y-2"
 						to={'/targeted-instruction/quizzes'}>
-						<img className="h-20 py-4" src={BlueQuiz} alt="img" />
+						<img className="w-20 h-20" src={BlueQuiz} alt="img" />
 						<div className="text-xs text-blue-900 font-bold">Quizzes</div>
 					</Link>
 					<Link
-						className="container sm:px-8 bg-white rounded-xl m-3 h-36 flex flex-col content-center items-center shadow-lg no-underline"
+						className="container sm:px-8 bg-white rounded-xl m-3 h-36 flex flex-col content-center items-center shadow-lg space-y-2"
 						to={'/targeted-instruction/results'}>
-						<img className="h-20 p-4" src={Results} alt="img" />
+						<img className="w-20 h-20" src={Results} alt="img" />
 						<div className="text-xs text-blue-900 font-bold">Results</div>
 					</Link>
 				</div>
@@ -113,15 +113,15 @@ const Home: React.FC<PropsType> = ({ faculty, faculty_id }) => {
 			<div className="flex flex-row content-center items-center justify-center w-full">
 				<div className="flex flex-row content-center items-center w-full">
 					<Link
-						className="container sm:px-8 bg-white rounded-xl m-3 h-36 flex flex-col content-center items-center shadow-lg no-underline"
+						className="container sm:px-8 bg-white rounded-xl m-3 h-36 flex flex-col content-center items-center shadow-lg space-y-2"
 						to={'/targeted-instruction/training-videos'}>
-						<img className="h-20 py-4" src={TrainingMaterials} alt="img" />
+						<img className="w-20 h-20" src={TrainingMaterials} alt="img" />
 						<div className="text-xs text-blue-900 font-bold">Training Materials</div>
 					</Link>
 					<Link
-						className="container sm:px-8 bg-white rounded-xl m-3 h-36 flex flex-col content-center items-center shadow-lg no-underline"
+						className="container sm:px-8 bg-white rounded-xl m-3 h-36 flex flex-col content-center items-center shadow-lg space-y-2"
 						to={'/targeted-instruction/lesson-plans'}>
-						<img className="h-20 p-4" src={LessonPlans} alt="img" />
+						<img className="w-20 h-20" src={LessonPlans} alt="img" />
 						<div className="text-xs text-blue-900 font-bold">Lesson Plans</div>
 					</Link>
 				</div>
