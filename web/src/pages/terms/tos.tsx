397e8ca9162c265cc7cb0c04a4e5bed7c68b49d5
Mudassar Ali <Mudassaralics007@gmail.com>
2021-06-25 12:31:26 +0500

    remove styles

--- web/src/pages/terms/tos.tsx
+++ web/src/pages/terms/tos.tsx
@@ -1,22 +1,16 @@
 import React from 'react'
-import './style.css'
 
 export const TermsOfService = () => (
-	<div className="p-5 pb-10 md:p-10 md:pb-0">
-		<p className="c15 title" id="h.ndjo9wiari45">
-			<span className="c8">Terms of Service</span>
+	<div className="p-5 pb-10 md:p-10 md:pb-0 space-y-4">
+		<p className="title text-3xl font-semibold">
+			<span>Terms of Service</span>
 		</p>
-		<p className="c14">
-			<span className="c6"></span>
+		<p>
+			<span>Last Update: 2021-07-01</span>
 		</p>
-		<p className="c12">
-			<span className="c2">Last Update: 2021-07-01</span>
-		</p>
-		<p className="c4">
-			<span className="c2"></span>
-		</p>
-		<p className="c12">
-			<span className="c2">
+
+		<p>
+			<span>
 				Please read these terms of service very carefully before registering for, or using,
 				the MISchool web site and Service. &nbsp;These terms of service
 				(&ldquo;Agreement&rdquo;) govern you accessing content and using MISchool.pk (and
@@ -28,24 +22,12 @@ export const TermsOfService = () => (
 				a binding agreement between you and MISchool.
 			</span>
 		</p>
-		<p className="c4">
-			<span className="c2"></span>
+		<p className="font-semibold">
+			<span>General Terms and Conditions</span>
 		</p>
-		<p className="c4">
-			<span className="c2"></span>
-		</p>
-		<p className="c12">
-			<span className="c13">General Terms and Conditions</span>
-		</p>
-		<p className="c4">
-			<span className="c13"></span>
-		</p>
-		<p className="c4">
-			<span className="c2"></span>
-		</p>
-		<ol className="c1 lst-kix_tsptkoxst6z8-0 start">
-			<li className="c0">
-				<span className="c2">
+		<ol className="list-decimal ml-4">
+			<li className="my-2">
+				<span>
 					This web site and any other site powered by MISchool (collectively the
 					&ldquo;Site&rdquo;) is owned and operated by CERP (referred to as &ldquo;Centre
 					for Economic Research in Pakistan&rdquo; or &ldquo;Company&rdquo; herein). The
@@ -56,8 +38,8 @@ export const TermsOfService = () => (
 					extent allowed by law.
 				</span>
 			</li>
-			<li className="c0">
-				<span className="c2">
+			<li className="my-2">
+				<span>
 					You acknowledge and agree that MISchool or Company may stop (permanently or
 					temporarily) providing the Services (or any features within the Services) to you
 					or to users generally at Company&rsquo;s sole discretion, without prior notice
@@ -68,8 +50,8 @@ export const TermsOfService = () => (
 					or any files or other materials which is contained in your account.
 				</span>
 			</li>
-			<li className="c0">
-				<span className="c2">
+			<li className="my-2">
+				<span>
 					You agree not to access (or attempt to access) any of the Services by any means
 					other than through the interface that is provided by Company unless you have
 					been specifically allowed to do so in a separate agreement with MISchool and/or
@@ -85,23 +67,23 @@ export const TermsOfService = () => (
 					such breach.
 				</span>
 			</li>
-			<li className="c0">
-				<span className="c2">
+			<li className="my-2">
+				<span>
 					The MISchool or Company do not charge for access using its mobile application;
 					however, your carrier rates will apply, such as fees for text messaging and data
 					charges. You hereby acknowledge, agree, and provide consent in order for users
 					to sync any device with your information.
 				</span>
 			</li>
-			<li className="c0">
-				<span className="c2">
+			<li className="my-2">
+				<span>
 					You understand that the MISchool website uses cookies and you hereby acknowledge
 					and agree that by using the website you consent to the use of cookies in
 					accordance with MISchool Privacy Policy.
 				</span>
 			</li>
-			<li className="c0">
-				<span className="c2">
+			<li className="my-2">
+				<span>
 					You understand that all media and information (such as data files, written text,
 					computer software, music, audio files or other sounds, photographs, videos or
 					other images) which you may have access to as part of, or through your use of,
@@ -109,29 +91,29 @@ export const TermsOfService = () => (
 					originated.
 				</span>
 			</li>
-			<li className="c0">
-				<span className="c2">
+			<li className="my-2">
+				<span>
 					The Company allows ALL organizations to link to the Site without prior written
 					approval.
 				</span>
 			</li>
-			<li className="c0">
-				<span className="c2">
+			<li className="my-2">
+				<span>
 					Without prior approval and express written permission from MISchool or Company,
 					you may not create frames around the Site or use other techniques that alter in
 					any way the visual presentation or appearance of the Site.
 				</span>
 			</li>
-			<li className="c0">
-				<span className="c2">
+			<li className="my-2">
+				<span>
 					While MISchool and Company use reasonable efforts to include accurate and
 					up-to-date information on the Site, MISchool and Company make no warranties or
 					representations as to its accuracy. MISchool and Company assume no liability or
 					responsibility for any errors or representations in the Content or this Site.
 				</span>
 			</li>
-			<li className="c0">
-				<span className="c2">
+			<li className="my-2">
+				<span>
 					The MISchool Site may contain links to other sites on the Internet which are
 					owned and operated by other third parties (the &ldquo;External Sites&rdquo;).
 					You acknowledge that MISchool and Company are not responsible for the
@@ -141,8 +123,8 @@ export const TermsOfService = () => (
 					External Sites.
 				</span>
 			</li>
-			<li className="c0">
-				<span className="c2">
+			<li className="my-2">
+				<span>
 					Users of the MISchool may post comments, reviews, and other materials and submit
 					suggestions, ideas, or other information, provided, the materials do not contain
 					any unlawful, harmful, threatening, abusive, harassing, defamatory, vulgar,
@@ -159,8 +141,8 @@ export const TermsOfService = () => (
 					including content that has been posted by users.
 				</span>
 			</li>
-			<li className="c0">
-				<span className="c2">
+			<li className="my-2">
+				<span>
 					At your discretion, you may provide feedback to MISchool and Company concerning
 					the functionality and performance of the Service from time to time, including,
 					without limitation, identifying potential errors, improvements, modifications,
@@ -176,8 +158,8 @@ export const TermsOfService = () => (
 					restriction.{' '}
 				</span>
 			</li>
-			<li className="c0">
-				<span className="c2">
+			<li className="my-2">
+				<span>
 					The Site may contain areas in which additional terms and conditions apply. For
 					purposes of the use of such areas, in the event of a conflict between the terms
 					and conditions of such other areas and these Terms and Conditions, the terms and
@@ -187,8 +169,8 @@ export const TermsOfService = () => (
 					review the current Terms and Conditions to which you are bound.
 				</span>
 			</li>
-			<li className="c0">
-				<span className="c2">
+			<li className="my-2">
+				<span>
 					You shall not transmit to MISchool or Company or upload to this Site any Harmful
 					Code or use or misappropriate the data on this Site for your own commercial
 					gain. &ldquo;Harmful Software&rdquo; shall mean any software (sometimes referred
@@ -207,13 +189,13 @@ export const TermsOfService = () => (
 					other person&rsquo;s knowledge and permission.
 				</span>
 			</li>
-			<li className="c0">
-				<span className="c2">
+			<li className="my-2">
+				<span>
 					You may not use your username and password for any unauthorized purpose.
 				</span>
 			</li>
-			<li className="c0">
-				<span className="c2">
+			<li className="my-2">
+				<span>
 					&nbsp;Provided such information does not individually identify you, MISchool and
 					Company may retain and use, information collected in your use of the Service.{' '}
 				</span>
