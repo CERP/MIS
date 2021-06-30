c94d6df75cfad76767e43a9ef0fb5b9b10309a9d
Mudassar Ali <Mudassaralics007@gmail.com>
2021-06-30 16:58:06 +0500

    refactor and add settings permission

--- web/src/utils/index.ts
+++ web/src/utils/index.ts
@@ -103,9 +103,12 @@ export const checkPermission = (
 		return true
 	}
 
+	let flag = false
+
 	switch (title) {
 		case 'salaries': {
-			return false
+			flag = false
+			break
 		}
 		case 'attendance':
 		case 'exams':
@@ -113,25 +116,33 @@ export const checkPermission = (
 		case 'SMS':
 		case 'diary':
 		case 'results': {
-			return tipAccess ? false : true
+			flag = tipAccess ? false : true
+			break
 		}
+		case 'settings':
 		case 'analytics':
 		case 'payments':
 		case 'certificates': {
-			return tipAccess ? false : subAdmin
+			flag = tipAccess ? false : subAdmin
+			break
 		}
 		case 'setup':
-		case 'dailyStats':
-			return (permissions.dailyStats || permissions.setupPage) && subAdmin
+		case 'dailyStats': {
+			flag = (permissions.dailyStats || permissions.setupPage) && subAdmin
+			break
+		}
 		case 'fees':
 		case 'expenses':
 		case 'families': {
-			return tipAccess
+			flag = tipAccess
 				? false
 				: (permissions.family || permissions.fee || permissions.expense) && subAdmin
+			break
 		}
-		default:
-			return true
+		default: {
+			flag = true
+			break
+		}
 	}
 }
 
