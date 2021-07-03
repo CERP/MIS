a2ab9226b2f7b2764542bffb697833f72c35fbad
Mudassar Ali <Mudassaralics007@gmail.com>
2021-07-03 15:42:40 +0500

    user hero-icons and css tweaks

--- web/src/pages/onboarding/student/index.tsx
+++ web/src/pages/onboarding/student/index.tsx
@@ -5,6 +5,7 @@ import { v4 } from 'node-uuid'
 import moment from 'moment'
 import clsx from 'clsx'
 import toast from 'react-hot-toast'
+import { DownloadIcon, TrashIcon, UploadIcon } from '@heroicons/react/outline'
 
 import downloadCSV from 'utils/downloadCSV'
 import { formatCNIC, formatPhone } from 'utils'
@@ -190,10 +191,10 @@ export const AddStudent: React.FC<AddStudentProps> = ({ skipStage }) => {
 
 	return (
 		<div className="md:w-4/5 md:mx-auto flex flex-col items-center rounded-2xl bg-gray-700 my-4 py-4 md:mt-8">
-			<div className="w-4/5 md:w-2/5 space-y-4">
-				<div className="text-white text-center text-base ">
+			<div className="w-full md:w-2/5 space-y-4 px-4">
+				<h1 className="text-white text-center text-base font-semibold">
 					Adding Students to {defaultSection?.namespaced_name}
-				</div>
+				</h1>
 
 				<div className="flex items-center flex-row">
 					<img
@@ -222,37 +223,13 @@ export const AddStudent: React.FC<AddStudentProps> = ({ skipStage }) => {
 								className="inline-flex w-full tw-btn bg-orange-brand text-white"
 								onClick={downloadCSVTemplate}>
 								{/* TODO: move these svg to separate component in separate file */}
-								<svg
-									className="w-5 h-5"
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor">
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth={2}
-										d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
-									/>
-								</svg>
+								<DownloadIcon className="w-5 h-5" />
 								<span className="mx-auto">Download Template</span>
 							</button>
 							<button
 								type="button"
 								className="inline-flex w-full tw-btn text-white bg-teal-brand">
-								<svg
-									className="w-5 h-5"
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor">
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth={2}
-										d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
-									/>
-								</svg>
+								<UploadIcon className="w-5 h-5" />
 								<label className="w-full mx-auto">
 									<input
 										type="file"
@@ -289,8 +266,8 @@ export const AddStudent: React.FC<AddStudentProps> = ({ skipStage }) => {
 														<div className="table-cell px-2">
 															{s.Phone}
 														</div>
-														<div className="table-cell p-2">
-															{/* TODO: add logic to render TModal here and display the students */}
+														{/* TODO: add TModal here */}
+														{/* <div className="table-cell p-2">
 															<svg
 																className="w-4 text-gray-400 mx-auto"
 																xmlns="http://www.w3.org/2000/svg"
@@ -298,7 +275,7 @@ export const AddStudent: React.FC<AddStudentProps> = ({ skipStage }) => {
 																fill="currentColor">
 																<path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
 															</svg>
-														</div>
+														</div> */}
 													</div>
 												))}
 										</div>
@@ -328,17 +305,7 @@ export const AddStudent: React.FC<AddStudentProps> = ({ skipStage }) => {
 											<div
 												className="table-cell p-2"
 												onClick={() => deleteStudent(s.id)}>
-												<svg
-													className="w-4 text-red-brand mx-auto"
-													xmlns="http://www.w3.org/2000/svg"
-													viewBox="0 0 20 20"
-													fill="currentColor">
-													<path
-														fillRule="evenodd"
-														d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-														clipRule="evenodd"
-													/>
-												</svg>
+												<TrashIcon className="text-red-brand mx-auto w-5 h-5 cursor-pointer" />
 											</div>
 										</div>
 									))}
